2370a3bde4b921a94b4c753cc06376d1c944dde5
PetyaYakimova <petya_yakimova@abv.bg>
2024-02-27 09:44:22 +0200

    Add IsEven custom validation attribute

---
+++ ChoreographyBuilder/Attributes/IsEven.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChoreographyBuilder.Attributes
+{
+	public class IsEven : ValidationAttribute
+	{
+		public IsEven()
+		{
+		}
+
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			if (value != null && (int)value % 2 != 0)
+			{
+				string errorMessage = FormatErrorMessage(validationContext.DisplayName);
+				return new ValidationResult(ErrorMessage);
+			}
+
+			return ValidationResult.Success;
+		}
+	}
+}
