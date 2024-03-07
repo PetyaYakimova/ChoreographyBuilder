6d88c623941dfe7dfeab910604c8f719d018dd63
PetyaYakimova <petya_yakimova@abv.bg>
2024-03-07 10:11:25 +0200

    Add unit of work pattern to work with db

---
+++ ChoreographyBuilder.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChoreographyBuilder.Infrastructure.Data.Common
+{
+	public class Repository : IRepository
+	{
+		private readonly DbContext context;
+
+		public Repository(ChoreographyBuilderDbContext context)
+		{
+			this.context = context;
+		}
+
+		private DbSet<T> DbSet<T>() where T : class
+		{
+			return context.Set<T>();
+		}
+
+		public IQueryable<T> All<T>() where T : class
+		{
+			return DbSet<T>();
+		}
+
+		public IQueryable<T> AllAsReadOnly<T>() where T : class
+		{
+			return DbSet<T>().AsNoTracking();
+		}
+	}
+}
