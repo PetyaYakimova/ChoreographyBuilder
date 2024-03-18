f1e68048343a2aa7ec1c478a72095648353003d9
PetyaYakimova <petya_yakimova@abv.bg>
2024-03-18 17:38:35 +0200

    Add position query models

---
+++ ChoreographyBuilder.Core/Models/Position/AllPositionsQueryModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChoreographyBuilder.Core.Models.Position
+{
+	public class AllPositionsQueryModel
+	{
+		[Display(Name = "Search by name")]
+		public string SearchItem { get; init; } = null!;
+
+		public int ItemsPerPage { get; init; } = 10;
+
+		public int CurrentPage { get; init; } = 1;
+
+		public int TotalItemCount { get; set; }
+
+		public IEnumerable<PositionTableViewModel> Positions { get; set; } = new List<PositionTableViewModel>();
+	}
+}
---
+++ ChoreographyBuilder.Core/Models/Position/PositionQueryServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChoreographyBuilder.Core.Models.Position
+{
+	public class PositionQueryServiceModel
+	{
+		public int TotalCount { get; set; }
+
+		public IEnumerable<PositionTableViewModel> Positions { get; set; } = new List<PositionTableViewModel>();
+	}
+}
