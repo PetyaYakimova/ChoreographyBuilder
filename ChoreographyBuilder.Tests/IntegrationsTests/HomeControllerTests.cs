cd9d0a4ffc154fa8ee42a62257366923ff35b756
PetyaYakimova <petya_yakimova@abv.bg>
2024-04-14 09:46:16 +0300

    Add home controller tests

---
+++ ChoreographyBuilder.Tests/IntegrationsTests/HomeControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using ChoreographyBuilder.Controllers;
+using ChoreographyBuilder.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ChoreographyBuilder.Tests.IntegrationsTests
+{
+	[TestFixture]
+	public class HomeControllerTests
+	{
+		private HomeController homeController;
+
+		[OneTimeSetUp]
+		public void OneTimeSetup()
+		{
+			this.homeController = new HomeController(UserServiceMock.Instance);
+		}
+
+		[Test]
+		[TestCase(400)]
+		[TestCase(401)]
+		[TestCase(403)]
+		[TestCase(404)]
+		[TestCase(500)]
+		public void Error_ShouldReturnView(int statusCode)
+		{
+			var result = this.homeController.Error(statusCode);
+
+			Assert.IsNotNull(result);
+
+			var viewResult = result as ViewResult;
+			Assert.IsNotNull(viewResult);
+		}
+
+		[Test]
+		public void Index_ShouldReturnViewForNonRegisteredUsers()
+		{
+			var result = this.homeController.Index();
+			Assert.IsNotNull(result);
+
+			var viewResult = result as ViewResult;
+			Assert.IsNotNull(viewResult);
+		}
+	}
+}
---
+++ ChoreographyBuilder.Tests/Mocks/UserServiceMock.cs
@@ -0,0 +1,29 @@
+﻿using ChoreographyBuilder.Core.Contracts;
+using ChoreographyBuilder.Core.Models.Statistics;
+using Moq;
+
+namespace ChoreographyBuilder.Tests.Mocks
+{
+	public class UserServiceMock
+	{
+		public static IUserService Instance
+		{
+			get
+			{
+				var userServiceMock = new Mock<IUserService>();
+
+
+				userServiceMock
+					.Setup(s => s.GetUserStatisticsAsync("userId"))
+					.ReturnsAsync(new UserStatisticModel()
+					{
+						MyTotalNumberOfFigures = 10,
+						MyTotalNumberOfVerseChoreographies = 3,
+						MyTotalNumberOfFullChoreographies = 1
+					});
+
+				return userServiceMock.Object;
+			}
+		}
+	}
+}
