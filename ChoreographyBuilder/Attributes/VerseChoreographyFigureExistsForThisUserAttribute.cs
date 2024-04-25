f4edc453bb071ced5dae3ab68a4d5f0f13ada2ba
PetyaYakimova <petya_yakimova@abv.bg>
2024-04-25 11:25:47 +0300

    Add attribute for verse choerography figure exists for this user

---
+++ ChoreographyBuilder/Attributes/VerseChoreographyFigureExistsForThisUserAttribute.cs
@@ -0,0 +1,44 @@
+﻿using ChoreographyBuilder.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace ChoreographyBuilder.Attributes
+{
+	public class VerseChoreographyFigureExistsForThisUserAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IVerseChoreographyFigureService? service = context.HttpContext.RequestServices.GetService<IVerseChoreographyFigureService>();
+
+			if (service == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			object? value = context.HttpContext.GetRouteData().Values["id"];
+			if (value == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+			}
+
+			if (value != null)
+			{
+				int id = 0;
+				if (int.TryParse(value.ToString(), out id))
+				{
+					if (service != null && service.VerseChoreographyFigureExistForThisUserByIdAsync(id, context.HttpContext.User.Id()).Result == false)
+					{
+						context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+					}
+				}
+				else
+				{
+					context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+				}
+			}
+		}
+	}
+}
