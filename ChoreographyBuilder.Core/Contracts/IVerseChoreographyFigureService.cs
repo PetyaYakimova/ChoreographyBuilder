c1b958d631a3f3f0ace213c18e884dc3f5546578
PetyaYakimova <petya_yakimova@abv.bg>
2024-04-25 11:25:23 +0300

    Add verse choreography figure service

---
+++ ChoreographyBuilder.Core/Contracts/IVerseChoreographyFigureService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
+
+namespace ChoreographyBuilder.Core.Contracts
+{
+	public interface IVerseChoreographyFigureService
+	{
+		/// <summary>
+		/// Returns the verse choreography figure replace model of a verse choreography figure with the selected id. 
+		/// Throws an exception if there is no such verse choreography figure with this id.
+		/// </summary>
+		/// <param name="verseChoreographyFigureId">Id of the verse choreography figure</param>
+		/// <returns></returns>
+		Task<VerseChoreographyFigureReplaceViewModel> GetVerseChoreographyFigureForReplaceAsync(int verseChoreographyFigureId);
+
+		/// <summary>
+		/// Returns a collection with all figures that could replace the given verse choreography figure.
+		/// Throws an exception if there is no such verse choreography figure with this id.
+		/// </summary>
+		/// <param name="verseChoreographyFigureId">Id of the verse choreography figure</param>
+		/// <returns></returns>
+		Task<IEnumerable<VerseChoreographyFigureViewModel>> GetPossibleReplacementsForVerseChoreographyFigureAsync(int verseChoreographyFigureId);
+
+		/// <summary>
+		/// Returns the verse choreography id of the given verse choreography figure. 
+		/// Throws an exception if there is no such verse choreography figure with this id.
+		/// </summary>
+		/// <param name="verseChoreographyFigureId">>Id of the verse choreography figure</param>
+		/// <returns></returns>
+		Task<int> GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(int verseChoreographyFigureId);
+
+		/// <summary>
+		/// Returns true if the verse choreography figure is linked to a verse choreography that is for this user. 
+		/// Returns false if the verse choreography figure doesn't exist at all, or if it is for another user.
+		/// </summary>
+		/// <param name="verseChoreographyFigureId">Id of the verse choreography figure</param>
+		/// <param name="userId">Id of the user</param>
+		/// <returns></returns>
+		Task<bool> VerseChoreographyFigureExistForThisUserByIdAsync(int verseChoreographyFigureId, string userId);
+	}
+}
---
+++ ChoreographyBuilder.Core/Services/VerseChoreographyFigureService.cs
@@ -0,0 +1,106 @@
+﻿using AutoMapper;
+using ChoreographyBuilder.Core.Contracts;
+using ChoreographyBuilder.Core.Exceptions;
+using ChoreographyBuilder.Core.Models.FigureOption;
+using ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
+using ChoreographyBuilder.Infrastructure.Data.Common;
+using ChoreographyBuilder.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using static ChoreographyBuilder.Core.Constants.MessageConstants;
+
+namespace ChoreographyBuilder.Core.Services
+{
+	public class VerseChoreographyFigureService : IVerseChoreographyFigureService
+	{
+		private readonly ILogger<VerseChoreographyFigureService> logger;
+		private readonly IRepository repository;
+		private readonly IMapper mapper;
+
+		public VerseChoreographyFigureService(ILogger<VerseChoreographyFigureService> logger, IRepository repository, IMapper mapper)
+		{
+			this.logger = logger;
+			this.repository = repository;
+			this.mapper = mapper;
+		}
+
+		public async Task<VerseChoreographyFigureReplaceViewModel> GetVerseChoreographyFigureForReplaceAsync(int verseChoreographyFigureId)
+		{
+			var option = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+				.Include(vcf => vcf.FigureOption)
+					.ThenInclude(fo => fo.Figure)
+				.Include(vcf => vcf.FigureOption)
+					.ThenInclude(fo => fo.StartPosition)
+				.Include(vcf => vcf.FigureOption)
+					.ThenInclude(fo => fo.EndPosition)
+				.FirstOrDefaultAsync(fo => fo.Id == verseChoreographyFigureId);
+
+			if (option == null)
+			{
+				logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+				throw new EntityNotFoundException();
+			}
+
+			return mapper.Map<VerseChoreographyFigureReplaceViewModel>(option);
+		}
+
+		public async Task<IEnumerable<VerseChoreographyFigureViewModel>> GetPossibleReplacementsForVerseChoreographyFigureAsync(int verseChoreographyFigureId)
+		{
+			var currentFigure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+				.Include(vcf => vcf.VerseChoreography)
+				.Include(vcf => vcf.FigureOption)
+				.FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
+
+			if (currentFigure == null)
+			{
+				logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+				throw new EntityNotFoundException();
+			}
+
+			var result = await repository.AllAsReadOnly<FigureOption>()
+				.Where(fo => fo.Figure.UserId == currentFigure.VerseChoreography.UserId)
+                .Where(fo=>fo.FigureId!=currentFigure.FigureOption.FigureId)
+				.Where(fo => fo.StartPositionId == currentFigure.FigureOption.StartPositionId)
+				.Where(fo => fo.EndPositionId == currentFigure.FigureOption.EndPositionId)
+				.Where(fo => fo.BeatCounts == currentFigure.FigureOption.BeatCounts)
+				.Include(f => f.Figure)
+				.Include(f => f.StartPosition)
+				.Include(f => f.EndPosition)
+				.ToListAsync();
+
+			return mapper.Map<List<VerseChoreographyFigureViewModel>>(result);
+		}
+
+		public async Task<int> GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(int verseChoreographyFigureId)
+		{
+			var verseChoreographyFigure = await repository.GetByIdAsync<VerseChoreographyFigure>(verseChoreographyFigureId);
+
+			if (verseChoreographyFigure == null)
+			{
+				logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+				throw new EntityNotFoundException();
+			}
+
+			return verseChoreographyFigure.VerseChoreographyId;
+		}
+
+		public async Task<bool> VerseChoreographyFigureExistForThisUserByIdAsync(int verseChoreographyFigureId, string userId)
+		{
+			VerseChoreographyFigure? verseChoreographyFigure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+				.Include(vcf => vcf.VerseChoreography)
+				.FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
+
+			if (verseChoreographyFigure == null)
+			{
+				return false;
+			}
+
+			if (verseChoreographyFigure.VerseChoreography.UserId != userId)
+			{
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
