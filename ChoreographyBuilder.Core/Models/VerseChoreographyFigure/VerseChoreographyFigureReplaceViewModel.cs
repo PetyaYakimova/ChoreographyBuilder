7d7c2a378516e10205037be33faad500d607e15d
PetyaYakimova <petya_yakimova@abv.bg>
2024-04-26 08:38:58 +0300

    Update verse choreography figure models and add ned ones

---
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureReplaceViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static ChoreographyBuilder.Core.Constants.MessageConstants;
+
+namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
+{
+	/// <summary>
+	/// View model for previewing a figure in verse choreography when replacing it. 
+	/// Added validation attributes.
+	/// </summary>
+	public class VerseChoreographyFigureReplaceViewModel : VerseChoreographyFigureViewModel
+	{
+		[Required(ErrorMessage = RequiredErrorMessage)]
+		[Display(Name = "New figure:")]
+		public int ReplacementFigureOptionId { get; set; }
+
+		public IEnumerable<VerseChoreographyFigureViewModel> PossibleReplacementFigures { get; set; } = new List<VerseChoreographyFigureViewModel>();
+	}
+}
---
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureSelectedReplacementServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
+{
+	/// <summary>
+	/// Service model for transferring the data for the selected new figure in the verse choreography.
+	/// No added validation attributes.
+	/// </summary>
+	public class VerseChoreographyFigureSelectedReplacementServiceModel
+	{
+		public int FigureOptionId { get; set; }
+
+		public int FigureOrder { get; set; }
+	}
+}
