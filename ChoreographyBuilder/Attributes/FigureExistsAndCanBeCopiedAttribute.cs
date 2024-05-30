46fc20c76a90a12512ec847ceef9c79eeff0ce0e
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-30 11:38:03 +0300

    Add attribute for figure can be copied

---
+++ ChoreographyBuilder/Attributes/FigureExistsAndCanBeCopiedAttribute.cs
@@ -0,0 +1,47 @@
+﻿using ChoreographyBuilder.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace ChoreographyBuilder.Attributes
+{
+	public class FigureExistsAndCanBeCopiedAttribute : ActionFilterAttribute
+	{
+		public override void OnActionExecuting(ActionExecutingContext context)
+		{
+			base.OnActionExecuting(context);
+
+			IFigureService? service = context.HttpContext.RequestServices.GetService<IFigureService>();
+
+			if (service == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+
+			object? value = context.HttpContext.GetRouteData().Values["id"];
+
+			if (value == null)
+			{
+				context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+			}
+
+			if (value != null)
+			{
+				int id = 0;
+				if (int.TryParse(value.ToString(), out id))
+				{
+					if (service != null &&
+						(service.FigureExistForThisUserByIdAsync(id, context.HttpContext.User.Id()).Result == false ||
+						service.FigureExistAndCanBeCopiedByIdAsync(id).Result == false))
+					{
+						context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+					}
+				}
+				else
+				{
+					context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+				}
+			}
+		}
+	}
+}
