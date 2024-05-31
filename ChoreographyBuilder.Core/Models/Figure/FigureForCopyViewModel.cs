f2e57b619bbe84cdc0f205b7072fb0b3a92769d1
PetyaYakimova <petya_yakimova@abv.bg>
2024-05-31 09:28:38 +0300

    Add figure for copy view model

---
+++ ChoreographyBuilder.Core/Models/Figure/FigureForCopyViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ChoreographyBuilder.Core.Models.Figure
+{
+	/// <summary>
+	/// View model only for previewing figures when copying a figure. 
+	/// No added validation attributes.
+	/// </summary>
+	public class FigureForCopyViewModel : FigureForPreviewViewModel
+	{
+		public int FigureOptionsCount { get; set; }
+
+		public string UserEmailAddress { get; set; } = null!;
+	}
+}
