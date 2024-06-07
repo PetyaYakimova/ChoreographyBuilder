7971fbd4991ff4388b34a368b767b6fc33395103
PetyaYakimova <petya_yakimova@abv.bg>
2024-06-07 09:37:53 +0300

    Update All figures query model to have properties for searching by figure option

--- ChoreographyBuilder.Core/Models/Figure/AllFiguresQueryModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/AllFiguresQueryModel.cs
@@ -1,4 +1,7 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
+using ChoreographyBuilder.Core.Models.Position;
+using ChoreographyBuilder.Infrastructure.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChoreographyBuilder.Core.Models.Figure
 {
@@ -8,5 +11,20 @@ namespace ChoreographyBuilder.Core.Models.Figure
 	/// </summary>
 	public class AllFiguresQueryModel : AllEntitiesQueryBaseModel <FigureTableViewModel>
 	{
+		[Display(Name = "Option start position")]
+		public int? StartPosition { get; init; }
+
+		[Display(Name = "Option end position")]
+		public int? EndPosition { get; init; }
+
+		[Display(Name = "Opion beats count")]
+		public int? BeatsCount { get; init; }
+
+		[Display(Name = "Option dynamics type")]
+		public DynamicsType? DynamicsType { get; init; }
+
+		public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
+
+		public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
 	}
 }
