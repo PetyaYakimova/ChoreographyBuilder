935ed17b4793829a4cb2ae53765f27575e0a330c
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-24 13:13:00 +0200

    Add verse choreography model

---
+++ ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ChoreographyBuilder.Core.Models.VerseType;
+using System.ComponentModel.DataAnnotations;
+using static ChoreographyBuilder.Core.Constants.MessageConstants;
+using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
+
+namespace ChoreographyBuilder.Core.Models.VerseChoreography
+{
+	/// <summary>
+	/// A model for generating by hand a verse choreography. 
+	/// Added validation attributes.
+	/// </summary>
+	public class VerseChoreographyFormViewModel
+	{
+		[Required(ErrorMessage = RequiredErrorMessage)]
+		[StringLength(VerseChoreographyNameMaxLength,
+			MinimumLength = VerseChoreographyNameMinLength,
+			ErrorMessage = StringLengthErrorMessage)]
+		public string Name { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = RequiredErrorMessage)]
+		public int VerseTypeId { get; init; }
+
+		public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
+	}
+}
