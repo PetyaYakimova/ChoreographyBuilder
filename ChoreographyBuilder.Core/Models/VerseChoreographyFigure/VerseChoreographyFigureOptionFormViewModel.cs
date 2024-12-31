a7db97da7a618bf884e703c3e8437eb68e3c8740
Petya Yakimova <petya_yakimova@abv.bg>
2024-12-31 10:55:16 +0200

    Add verse choreography figure option form view model

---
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureOptionFormViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ChoreographyBuilder.Core.Models.FigureOption;
+using System.ComponentModel.DataAnnotations;
+using static ChoreographyBuilder.Core.Constants.MessageConstants;
+
+namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
+{
+    /// <summary>
+	/// View model only for when selecting figure option for verse choreography. 
+	/// Added validation attributes.
+	/// </summary>
+	public class VerseChoreographyFigureOptionFormViewModel
+    {
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Figure option:")]
+        public int FigureOptionId { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [Display(Name = "Order:")]
+        public int FigureOrder { get; set; }
+
+        [Display(Name = "Start position:")]
+        public string? StartPositionName { get; set; }
+
+        //TODO: Change it with model that has the figure name in it as well
+        public IEnumerable<FigureOptionTableViewModel> Figures { get; set; } = new List<FigureOptionTableViewModel>();
+    }
+}
