f4f41a139d35c8a1affcc1ea6a8117e7db9dd6e2
Petya Yakimova <petya_yakimova@abv.bg>
2025-01-01 13:41:31 +0200

    Add figure option with figure name view model

---
+++ ChoreographyBuilder.Core/Models/FigureOption/FigureOptionWithFigureViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ChoreographyBuilder.Core.Models.FigureOption
+{
+    /// <summary>
+    /// View model for previewing figure option when selecting one for verse choreography.
+    /// No added validation attributes.
+    /// </summary>
+    public class FigureOptionWithFigureViewModel : FigureOptionTableViewModel
+    {
+        public string FigureName { get; set; } = string.Empty;
+    }
+}
