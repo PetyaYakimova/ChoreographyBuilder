8dc5a65d59f1ee5c510fa54e0054a097184a5fa1
Petya Yakimova <petya_yakimova@abv.bg>
2025-01-25 09:54:21 +0200

    Add verse choreography figure delete view model

---
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureDeleteViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
+{
+    /// <summary>
+	/// View model for previewing figure in verse choreography when trying to delete it. 
+	/// No added validation attributes.
+	/// </summary>
+    public class VerseChoreographyFigureDeleteViewModel
+    {
+        public int Id { get; set; }
+
+        public string FigureName { get; set; } = string.Empty;
+
+        public string VerseChoreographyName { get; set; } = string.Empty;
+
+        public int VerseChoreographyId { get; set; }
+    }
+}
