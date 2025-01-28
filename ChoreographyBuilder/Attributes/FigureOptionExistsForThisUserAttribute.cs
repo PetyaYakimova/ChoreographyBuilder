239dc76e06df68ece9417311e99c59b59d0b0d85
Petya Yakimova <petya_yakimova@abv.bg>
2025-01-28 09:11:38 +0200

    Update namespaces

--- ChoreographyBuilder/Attributes/FigureOptionExistsForThisUserAttribute.cs
+++ ChoreographyBuilder/Attributes/FigureOptionExistsForThisUserAttribute.cs
@@ -3,42 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Security.Claims;
 
-namespace ChoreographyBuilder.Attributes
+namespace ChoreographyBuilder.Attributes;
+
+public class FigureOptionExistsForThisUserAttribute : ActionFilterAttribute
 {
-	public class FigureOptionExistsForThisUserAttribute : ActionFilterAttribute
-	{
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			base.OnActionExecuting(context);
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
 
-			IFigureOptionService? service = context.HttpContext.RequestServices.GetService<IFigureOptionService>();
+        IFigureOptionService? service = context.HttpContext.RequestServices.GetService<IFigureOptionService>();
 
-			if (service == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
-			}
+        if (service == null)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
 
-			object? value = context.HttpContext.GetRouteData().Values["id"];
-			if (value == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
-			}
+        object? value = context.HttpContext.GetRouteData().Values["id"];
+        if (value == null)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+        }
 
-			if (value != null)
-			{
-				int id = 0;
-				if (int.TryParse(value.ToString(), out id))
-				{
-					if (service != null && service.FigureOptionExistForThisUserByIdAsync(id, context.HttpContext.User.Id()).Result == false)
-					{
-						context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
-					}
-				}
-				else
-				{
-					context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
-				}
-			}
-		}
-	}
+        if (value != null)
+        {
+            int id = 0;
+            if (int.TryParse(value.ToString(), out id))
+            {
+                if (service != null && service.FigureOptionExistForThisUserByIdAsync(id, context.HttpContext.User.Id()).Result == false)
+                {
+                    context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+                }
+            }
+            else
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+        }
+    }
 }
