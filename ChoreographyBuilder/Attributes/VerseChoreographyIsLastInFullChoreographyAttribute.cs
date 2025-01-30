a329836ec0380c1c937cf91593f17ff256b9ee7c
Petya Yakimova <petya_yakimova@abv.bg>
2025-01-30 09:11:01 +0200

    Update namespaces

--- ChoreographyBuilder/Attributes/VerseChoreographyIsLastInFullChoreographyAttribute.cs
+++ ChoreographyBuilder/Attributes/VerseChoreographyIsLastInFullChoreographyAttribute.cs
@@ -2,42 +2,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace ChoreographyBuilder.Attributes
+namespace ChoreographyBuilder.Attributes;
+
+public class VerseChoreographyIsLastInFullChoreographyAttribute : ActionFilterAttribute
 {
-	public class VerseChoreographyIsLastInFullChoreographyAttribute : ActionFilterAttribute
-	{
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			base.OnActionExecuting(context);
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
 
-			IFullChoreographyVerseChoreographyService? service = context.HttpContext.RequestServices.GetService<IFullChoreographyVerseChoreographyService>();
+        IFullChoreographyVerseChoreographyService? service = context.HttpContext.RequestServices.GetService<IFullChoreographyVerseChoreographyService>();
 
-			if (service == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
-			}
+        if (service == null)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
 
-			object? value = context.HttpContext.GetRouteData().Values["id"];
-			if (value == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
-			}
+        object? value = context.HttpContext.GetRouteData().Values["id"];
+        if (value == null)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+        }
 
-			if (value != null)
-			{
-				int id = 0;
-				if (int.TryParse(value.ToString(), out id))
-				{
-					if (service != null && service.VerseChoreographyIsLastForFullChoreographyByIdAsync(id).Result == false)
-					{
-						context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
-					}
-				}
-				else
-				{
-					context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
-				}
-			}
-		}
-	}
+        if (value != null)
+        {
+            int id = 0;
+            if (int.TryParse(value.ToString(), out id))
+            {
+                if (service != null && service.VerseChoreographyIsLastForFullChoreographyByIdAsync(id).Result == false)
+                {
+                    context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+                }
+            }
+            else
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+        }
+    }
 }
--- ChoreographyBuilder/Attributes/VerseChoreographyNotUsedInFullChoreographiesAttribute.cs
+++ ChoreographyBuilder/Attributes/VerseChoreographyNotUsedInFullChoreographiesAttribute.cs
@@ -2,42 +2,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
-namespace ChoreographyBuilder.Attributes
+namespace ChoreographyBuilder.Attributes;
+
+public class VerseChoreographyNotUsedInFullChoreographiesAttribute : ActionFilterAttribute
 {
-	public class VerseChoreographyNotUsedInFullChoreographiesAttribute : ActionFilterAttribute
-	{
-		public override void OnActionExecuting(ActionExecutingContext context)
-		{
-			base.OnActionExecuting(context);
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        base.OnActionExecuting(context);
 
-			IVerseChoreographyService? service = context.HttpContext.RequestServices.GetService<IVerseChoreographyService>();
+        IVerseChoreographyService? service = context.HttpContext.RequestServices.GetService<IVerseChoreographyService>();
 
-			if (service == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
-			}
+        if (service == null)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
 
-			object? value = context.HttpContext.GetRouteData().Values["id"];
-			if (value == null)
-			{
-				context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
-			}
+        object? value = context.HttpContext.GetRouteData().Values["id"];
+        if (value == null)
+        {
+            context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+        }
 
-			if (value != null)
-			{
-				int id = 0;
-				if (int.TryParse(value.ToString(), out id))
-				{
-					if (service != null && service.IsVerseChoreographyUsedInFullChoreographiesAsync(id).Result)
-					{
-						context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
-					}
-				}
-				else
-				{
-					context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
-				}
-			}
-		}
-	}
+        if (value != null)
+        {
+            int id = 0;
+            if (int.TryParse(value.ToString(), out id))
+            {
+                if (service != null && service.IsVerseChoreographyUsedInFullChoreographiesAsync(id).Result)
+                {
+                    context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+                }
+            }
+            else
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status404NotFound);
+            }
+        }
+    }
 }
