c5988893ea42942dd061ebc6a7b9eede66ab145f
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-03 09:22:55 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Contracts/IFullChoreographyService.cs
+++ ChoreographyBuilder.Core/Contracts/IFullChoreographyService.cs
@@ -2,93 +2,92 @@
 using ChoreographyBuilder.Core.Models.Position;
 using static ChoreographyBuilder.Core.Constants.LimitConstants;
 
-namespace ChoreographyBuilder.Core.Contracts
+namespace ChoreographyBuilder.Core.Contracts;
+
+public interface IFullChoreographyService
 {
-	public interface IFullChoreographyService
-	{
-		/// <summary>
-		/// Returns FullChoreographyDetailsViewModel for the choreography with the selected id.
-		/// Throws an exception if there is no such choreography with this id.
-		/// </summary>
-		/// <param name="id">Id of the full choreography</param>
-		/// <returns></returns>
-		Task<FullChoreographyDetailsViewModel> GetChoreographyDetailsByIdAsync(int id);
+	/// <summary>
+	/// Returns FullChoreographyDetailsViewModel for the choreography with the selected id.
+	/// Throws an exception if there is no such choreography with this id.
+	/// </summary>
+	/// <param name="id">Id of the full choreography</param>
+	/// <returns></returns>
+	Task<FullChoreographyDetailsViewModel> GetChoreographyDetailsByIdAsync(int id);
 
-		/// <summary>
-		/// Returns FullChoreographyFormViewModel for the choreography with the selected id.
-		/// Throws an exception if there is no such choreography with this id.
-		/// </summary>
-		/// <param name="id">Id of the full choreography</param>
-		/// <returns></returns>
-		Task<FullChoreographyFormViewModel> GetChoreographyForEditByIdAsync(int id);
+	/// <summary>
+	/// Returns FullChoreographyFormViewModel for the choreography with the selected id.
+	/// Throws an exception if there is no such choreography with this id.
+	/// </summary>
+	/// <param name="id">Id of the full choreography</param>
+	/// <returns></returns>
+	Task<FullChoreographyFormViewModel> GetChoreographyForEditByIdAsync(int id);
 
-		/// <summary>
-		/// Returns FullChoreographyTableViewModel for the choreography with the selected id.
-		/// Throws an exception if there is no such choreography with this id.
-		/// </summary>
-		/// <param name="id">Id of the full choreography</param>
-		/// <returns></returns>
-		Task<FullChoreographyTableViewModel> GetFullChoreographyForDeleteAsync(int id);
+	/// <summary>
+	/// Returns FullChoreographyTableViewModel for the choreography with the selected id.
+	/// Throws an exception if there is no such choreography with this id.
+	/// </summary>
+	/// <param name="id">Id of the full choreography</param>
+	/// <returns></returns>
+	Task<FullChoreographyTableViewModel> GetFullChoreographyForDeleteAsync(int id);
 
-		/// <summary>
-		/// Returns the end position of the last verse choreography in the verse choreography. 
-		/// Returns null if the full choreography doesn't have any verse choreographies or if the full choreography doesn't exist at all.
-		/// </summary>
-		/// <param name="fullChoreographyId">Id of the full choreography</param>
-		/// <returns></returns>
-		Task<PositionForPreviewViewModel?> GetLastVerseChoreographyEndPositionAsync(int fullChoreographyId);
+	/// <summary>
+	/// Returns the end position of the last verse choreography in the verse choreography. 
+	/// Returns null if the full choreography doesn't have any verse choreographies or if the full choreography doesn't exist at all.
+	/// </summary>
+	/// <param name="fullChoreographyId">Id of the full choreography</param>
+	/// <returns></returns>
+	Task<PositionForPreviewViewModel?> GetLastVerseChoreographyEndPositionAsync(int fullChoreographyId);
 
-		/// <summary>
-		/// Returns the number of verse choreographies in the full choreography.
-		/// Throws an exception if there is no such choreography with this id.
-		/// </summary>
-		/// <param name="fullChoreographyId">Id of the full choreography</param>
-		/// <returns></returns>
-		Task<int> GetNumberOfVerseChoreographiesForFullChoreographyAsync(int fullChoreographyId);
+	/// <summary>
+	/// Returns the number of verse choreographies in the full choreography.
+	/// Throws an exception if there is no such choreography with this id.
+	/// </summary>
+	/// <param name="fullChoreographyId">Id of the full choreography</param>
+	/// <returns></returns>
+	Task<int> GetNumberOfVerseChoreographiesForFullChoreographyAsync(int fullChoreographyId);
 
-		/// <summary>
-		/// Gets the full choreographies for for the user by the selected search criteria and returns only those of them that should be displayed on the given page.
-		/// </summary>
-		/// <param name="userId">Id of the user</param>
-		/// <param name="searchTerm"></param>
-		/// <param name="currentPage"></param>
-		/// <param name="itemsPerPage"></param>
-		/// <returns></returns>
-		Task<FullChoreographyQueryServiceModel> AllUserFullChoreographiesAsync(string userId, string? searchTerm = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
+	/// <summary>
+	/// Gets the full choreographies for for the user by the selected search criteria and returns only those of them that should be displayed on the given page.
+	/// </summary>
+	/// <param name="userId">Id of the user</param>
+	/// <param name="searchTerm"></param>
+	/// <param name="currentPage"></param>
+	/// <param name="itemsPerPage"></param>
+	/// <returns></returns>
+	Task<FullChoreographyQueryServiceModel> AllUserFullChoreographiesAsync(string userId, string? searchTerm = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
 
-		/// <summary>
-		/// Returns true if the full choreography is for this user.
-		/// Returns false if the full choreography doesn't exist at all, or if it is for another user.
-		/// </summary>
-		/// <param name="id">Id of the full choreography</param>
-		/// <param name="userId">Id of the user</param>
-		/// <returns></returns>
-		Task<bool> FullChoreographyExistForThisUserByIdAsync(int id, string userId);
+	/// <summary>
+	/// Returns true if the full choreography is for this user.
+	/// Returns false if the full choreography doesn't exist at all, or if it is for another user.
+	/// </summary>
+	/// <param name="id">Id of the full choreography</param>
+	/// <param name="userId">Id of the user</param>
+	/// <returns></returns>
+	Task<bool> FullChoreographyExistForThisUserByIdAsync(int id, string userId);
 
-		/// <summary>
-		/// Adds a new full choreography for the user with data from the model.
-		/// Throws an exception if a user with the given id doesn't exist.
-		/// </summary>
-		/// <param name="model">FullChoreographyFormViewModel model</param>
-		/// <param name="userId">Id of the user</param>
-		/// <returns></returns>
-		Task<int> AddFullChoreographyAsync(FullChoreographyFormViewModel model, string userId);
+	/// <summary>
+	/// Adds a new full choreography for the user with data from the model.
+	/// Throws an exception if a user with the given id doesn't exist.
+	/// </summary>
+	/// <param name="model">FullChoreographyFormViewModel model</param>
+	/// <param name="userId">Id of the user</param>
+	/// <returns></returns>
+	Task<int> AddFullChoreographyAsync(FullChoreographyFormViewModel model, string userId);
 
-		/// <summary>
-		/// Edits the full choreography with the given id with the updated data from the model.
-		/// Throws an exception if there is no such full choreography with this id.
-		/// </summary>
-		/// <param name="id">Id of the full choreography</param>
-		/// <param name="model">FullChoreographyFormViewModel model</param>
-		/// <returns></returns>
-		Task EditFullChoreographyAsync(int id, FullChoreographyFormViewModel model);
+	/// <summary>
+	/// Edits the full choreography with the given id with the updated data from the model.
+	/// Throws an exception if there is no such full choreography with this id.
+	/// </summary>
+	/// <param name="id">Id of the full choreography</param>
+	/// <param name="model">FullChoreographyFormViewModel model</param>
+	/// <returns></returns>
+	Task EditFullChoreographyAsync(int id, FullChoreographyFormViewModel model);
 
-		/// <summary>
-		/// Deletes a full choreography by its id.
-		/// </summary>
-		/// <param name="id">Id of the full choreography</param>
-		/// <returns></returns>
-		Task DeleteFullChoreographyAsync(int id);
+	/// <summary>
+	/// Deletes a full choreography by its id.
+	/// </summary>
+	/// <param name="id">Id of the full choreography</param>
+	/// <returns></returns>
+	Task DeleteFullChoreographyAsync(int id);
 
-	}
 }
--- ChoreographyBuilder.Core/Contracts/IFullChoreographyVerseChoreographyService.cs
+++ ChoreographyBuilder.Core/Contracts/IFullChoreographyVerseChoreographyService.cs
@@ -1,49 +1,48 @@
 ﻿using ChoreographyBuilder.Core.Models.FullChoreographyVerseChoreography;
 
-namespace ChoreographyBuilder.Core.Contracts
+namespace ChoreographyBuilder.Core.Contracts;
+
+public interface IFullChoreographyVerseChoreographyService
 {
-	public interface IFullChoreographyVerseChoreographyService
-	{
-		/// <summary>
-		/// Returns VerseChoreographyFullChoreographyDeleteViewModel of a full choreography verse choreography mapping record with the selected id. 
-		/// Throws an exception if there is no such full choreography verse choreography record with this id.
-		/// </summary>
-		/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
-		/// <returns></returns>
-		Task<FullChoreographyVerseChoreographyDeleteViewModel> GetVerseChoreographyForDeleteAsync(int fullChoreographyVerseChoreographyId);
+	/// <summary>
+	/// Returns VerseChoreographyFullChoreographyDeleteViewModel of a full choreography verse choreography mapping record with the selected id. 
+	/// Throws an exception if there is no such full choreography verse choreography record with this id.
+	/// </summary>
+	/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
+	/// <returns></returns>
+	Task<FullChoreographyVerseChoreographyDeleteViewModel> GetVerseChoreographyForDeleteAsync(int fullChoreographyVerseChoreographyId);
 
-		/// <summary>
-		/// Returns true if the full choreography verse choreography record is for full choreography that is for this user.
-		/// Returns false if the full choreography verse choreography record doesn't exist at all, or if the full choreography is for another user.
-		/// </summary>
-		/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
-		/// <param name="userId">Id of the user</param>
-		/// <returns></returns>
-		Task<bool> VerseChoreographyInFullChoreographyExistForThisUserByIdAsync(int fullChoreographyVerseChoreographyId, string userId);
+	/// <summary>
+	/// Returns true if the full choreography verse choreography record is for full choreography that is for this user.
+	/// Returns false if the full choreography verse choreography record doesn't exist at all, or if the full choreography is for another user.
+	/// </summary>
+	/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
+	/// <param name="userId">Id of the user</param>
+	/// <returns></returns>
+	Task<bool> VerseChoreographyInFullChoreographyExistForThisUserByIdAsync(int fullChoreographyVerseChoreographyId, string userId);
 
-		/// <summary>
-		/// Returns true if the full choreography verse choreography record is the last in order in the full choreography.
-		/// Returns false if the full choreography verse choreography record doesn't exist at all, or if is not the last for the full choreography.
-		/// </summary>
-		/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
-		/// <returns></returns>
-		Task<bool> VerseChoreographyIsLastForFullChoreographyByIdAsync(int fullChoreographyVerseChoreographyId);
+	/// <summary>
+	/// Returns true if the full choreography verse choreography record is the last in order in the full choreography.
+	/// Returns false if the full choreography verse choreography record doesn't exist at all, or if is not the last for the full choreography.
+	/// </summary>
+	/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
+	/// <returns></returns>
+	Task<bool> VerseChoreographyIsLastForFullChoreographyByIdAsync(int fullChoreographyVerseChoreographyId);
 
-		/// <summary>
-		/// Adds a new full choreography verse choreography record with data from the model.
-		/// Throws an exception if a full choreography with the given id doesn't exist or if the given verse choreography id doesn't exist.
-		/// Throws an exception if the user of the full choreography and the verse choreography are not the same.
-		/// </summary>
-		/// <param name="fullChoreographyId">Id of the full choreography</param>
-		/// <param name="model"></param>
-		/// <returns></returns>
-		Task AddVerseChoreographyToFullChoreographyAsync(int fullChoreographyId, FullChoreographyVerseChoreographyFormViewModel model);
+	/// <summary>
+	/// Adds a new full choreography verse choreography record with data from the model.
+	/// Throws an exception if a full choreography with the given id doesn't exist or if the given verse choreography id doesn't exist.
+	/// Throws an exception if the user of the full choreography and the verse choreography are not the same.
+	/// </summary>
+	/// <param name="fullChoreographyId">Id of the full choreography</param>
+	/// <param name="model"></param>
+	/// <returns></returns>
+	Task AddVerseChoreographyToFullChoreographyAsync(int fullChoreographyId, FullChoreographyVerseChoreographyFormViewModel model);
 
-		/// <summary>
-		/// Deletes a full choreography verse choreography record by its id.
-		/// </summary>
-		/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
-		/// <returns></returns>
-		Task DeleteVerseChoreographyFromFullChoreographyAsync(int fullChoreographyVerseChoreographyId);
-	}
+	/// <summary>
+	/// Deletes a full choreography verse choreography record by its id.
+	/// </summary>
+	/// <param name="fullChoreographyVerseChoreographyId">Id of the full choreography verse choreography mapping record</param>
+	/// <returns></returns>
+	Task DeleteVerseChoreographyFromFullChoreographyAsync(int fullChoreographyVerseChoreographyId);
 }
