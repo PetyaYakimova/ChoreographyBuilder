9b333336dae23a5ac7086f68653d0ca8c573da9f
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-04 09:23:07 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Contracts/IVerseChoreographyFigureService.cs
+++ ChoreographyBuilder.Core/Contracts/IVerseChoreographyFigureService.cs
@@ -1,9 +1,8 @@
-﻿using ChoreographyBuilder.Core.Models.FullChoreographyVerseChoreography;
-using ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
+﻿using ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
 
-namespace ChoreographyBuilder.Core.Contracts
-{
-    public interface IVerseChoreographyFigureService
+namespace ChoreographyBuilder.Core.Contracts;
+
+public interface IVerseChoreographyFigureService
 	{
 		/// <summary>
 		/// Returns the verse choreography figure replace model of a verse choreography figure with the selected id. 
@@ -21,21 +20,21 @@ namespace ChoreographyBuilder.Core.Contracts
 		/// <returns></returns>
 		Task<IEnumerable<VerseChoreographyFigureViewModel>> GetPossibleReplacementsForVerseChoreographyFigureAsync(int verseChoreographyFigureId);
 
-        /// <summary>
-        /// Returns VerseChoreographyFigureDeleteViewModel of a verse choreography figure mapping record with the selected id. 
-        /// Throws an exception if there is no such verse choreography figure record with this id.
-        /// </summary>
-        /// <param name="verseChoreographyFigureId">Id of the verse choreography figure mapping record</param>
-        /// <returns></returns>
-        Task<VerseChoreographyFigureDeleteViewModel> GetFigureForDeleteAsync(int verseChoreographyFigureId);
+    /// <summary>
+    /// Returns VerseChoreographyFigureDeleteViewModel of a verse choreography figure mapping record with the selected id. 
+    /// Throws an exception if there is no such verse choreography figure record with this id.
+    /// </summary>
+    /// <param name="verseChoreographyFigureId">Id of the verse choreography figure mapping record</param>
+    /// <returns></returns>
+    Task<VerseChoreographyFigureDeleteViewModel> GetFigureForDeleteAsync(int verseChoreographyFigureId);
 
-        /// <summary>
-        /// Returns the verse choreography id of the given verse choreography figure. 
-        /// Throws an exception if there is no such verse choreography figure with this id.
-        /// </summary>
-        /// <param name="verseChoreographyFigureId">>Id of the verse choreography figure</param>
-        /// <returns></returns>
-        Task<int> GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(int verseChoreographyFigureId);
+    /// <summary>
+    /// Returns the verse choreography id of the given verse choreography figure. 
+    /// Throws an exception if there is no such verse choreography figure with this id.
+    /// </summary>
+    /// <param name="verseChoreographyFigureId">>Id of the verse choreography figure</param>
+    /// <returns></returns>
+    Task<int> GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(int verseChoreographyFigureId);
 
 		/// <summary>
 		/// Returns true if the verse choreography figure is linked to a verse choreography that is for this user. 
@@ -46,29 +45,28 @@ namespace ChoreographyBuilder.Core.Contracts
 		/// <returns></returns>
 		Task<bool> VerseChoreographyFigureExistForThisUserByIdAsync(int verseChoreographyFigureId, string userId);
 
-        /// <summary>
-        /// Returns true if the verse choreography figure record is the last in order in the verse choreography.
-        /// Returns false if the verse choreography figure record doesn't exist at all, or if is not the last for the verse choreography.
-        /// </summary>
-        /// <param name="verseChoreographyFigureId">Id of the verse choreography figure mapping record</param>
-        /// <returns></returns>
-        Task<bool> FigureIsLastForVerseChoreographyByIdAsync(int verseChoreographyFigureId);
+    /// <summary>
+    /// Returns true if the verse choreography figure record is the last in order in the verse choreography.
+    /// Returns false if the verse choreography figure record doesn't exist at all, or if is not the last for the verse choreography.
+    /// </summary>
+    /// <param name="verseChoreographyFigureId">Id of the verse choreography figure mapping record</param>
+    /// <returns></returns>
+    Task<bool> FigureIsLastForVerseChoreographyByIdAsync(int verseChoreographyFigureId);
 
-        /// <summary>
-        /// Adds a new verse choreography figure record with data from the model.
-        /// Throws an exception if a verse choreography with the given id doesn't exist or if the given figure option id doesn't exist.
-        /// Throws an exception if the user of the verse choreography and the figure option are not the same.
-        /// </summary>
-        /// <param name="verseChoreographyId">Id of the verse choreography</param>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        Task AddFigureToVerseChoreographyAsync(int verseChoreographyId, VerseChoreographyFigureOptionFormViewModel model);
+    /// <summary>
+    /// Adds a new verse choreography figure record with data from the model.
+    /// Throws an exception if a verse choreography with the given id doesn't exist or if the given figure option id doesn't exist.
+    /// Throws an exception if the user of the verse choreography and the figure option are not the same.
+    /// </summary>
+    /// <param name="verseChoreographyId">Id of the verse choreography</param>
+    /// <param name="model"></param>
+    /// <returns></returns>
+    Task AddFigureToVerseChoreographyAsync(int verseChoreographyId, VerseChoreographyFigureOptionFormViewModel model);
 
-        /// <summary>
+    /// <summary>
 		/// Deletes a verse choreography figure record by its id.
 		/// </summary>
 		/// <param name="verseChoreographyFigureId">Id of the verse choreography figure mapping record</param>
 		/// <returns></returns>
 		Task DeleteFigureFromVerseChoreographyAsync(int verseChoreographyFigureId);
-    }
 }
--- ChoreographyBuilder.Core/Contracts/IVerseTypeService.cs
+++ ChoreographyBuilder.Core/Contracts/IVerseTypeService.cs
@@ -1,91 +1,89 @@
-﻿using ChoreographyBuilder.Core.Models.VerseChoreography;
-using ChoreographyBuilder.Core.Models.VerseType;
+﻿using ChoreographyBuilder.Core.Models.VerseType;
 using static ChoreographyBuilder.Core.Constants.LimitConstants;
 
-namespace ChoreographyBuilder.Core.Contracts
+namespace ChoreographyBuilder.Core.Contracts;
+
+public interface IVerseTypeService
 {
-	public interface IVerseTypeService
-	{
-		/// <summary>
-		/// Returns VerseTypeFormViewModel for the verse type with the selected id.
-		/// Throws an exception if there is no such verse type with this id.
-		/// </summary>
-		/// <param name="id">Id of the verse type</param>
-		/// <returns></returns>
-		Task<VerseTypeFormViewModel> GetVerseTypeById(int id);
+	/// <summary>
+	/// Returns VerseTypeFormViewModel for the verse type with the selected id.
+	/// Throws an exception if there is no such verse type with this id.
+	/// </summary>
+	/// <param name="id">Id of the verse type</param>
+	/// <returns></returns>
+	Task<VerseTypeFormViewModel> GetVerseTypeById(int id);
 
-		/// <summary>
-		/// Returns VerseTypeForPreviewViewModel for the verse type with the selected id.
-		/// Throws an exception if there is no such verse type with this id.
-		/// </summary>
-		/// <param name="id">Id of the verse type</param>
-		/// <returns></returns>
-		Task<VerseTypeForPreviewViewModel> GetVerseTypeForDeleteAsync(int id);
+	/// <summary>
+	/// Returns VerseTypeForPreviewViewModel for the verse type with the selected id.
+	/// Throws an exception if there is no such verse type with this id.
+	/// </summary>
+	/// <param name="id">Id of the verse type</param>
+	/// <returns></returns>
+	Task<VerseTypeForPreviewViewModel> GetVerseTypeForDeleteAsync(int id);
 
-		/// <summary>
-		/// Gets the verse types by the selected search criteria and returns only those of them that should be displayed on the given page.
-		/// </summary>
-		/// <param name="searchTerm"></param>
-		/// <param name="searchedBeatsCount"></param>
-		/// <param name="currentPage"></param>
-		/// <param name="itemsPerPage"></param>
-		/// <returns></returns>
-		Task<VerseTypeQueryServiceModel> AllVerseTypesAsync(string? searchTerm = null, int? searchedBeatsCount = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
+	/// <summary>
+	/// Gets the verse types by the selected search criteria and returns only those of them that should be displayed on the given page.
+	/// </summary>
+	/// <param name="searchTerm"></param>
+	/// <param name="searchedBeatsCount"></param>
+	/// <param name="currentPage"></param>
+	/// <param name="itemsPerPage"></param>
+	/// <returns></returns>
+	Task<VerseTypeQueryServiceModel> AllVerseTypesAsync(string? searchTerm = null, int? searchedBeatsCount = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage);
 
-		/// <summary>
-		/// Returns a collection with all the active verse types and the verse type with the given id even if it is not active.
-		/// </summary>
-		/// <param name="selectedVerseTypeId">Id of the selected verse type</param>
-		/// <returns></returns>
-		Task<IEnumerable<VerseTypeForPreviewViewModel>> AllActiveVerseTypesOrSelectedVerseTypeAsync(int? selectedVerseTypeId = null);
+	/// <summary>
+	/// Returns a collection with all the active verse types and the verse type with the given id even if it is not active.
+	/// </summary>
+	/// <param name="selectedVerseTypeId">Id of the selected verse type</param>
+	/// <returns></returns>
+	Task<IEnumerable<VerseTypeForPreviewViewModel>> AllActiveVerseTypesOrSelectedVerseTypeAsync(int? selectedVerseTypeId = null);
 
-		/// <summary>
-		/// Returns true if the verse type exists.
-		/// Returns false if there is no such verse type with this id.
-		/// </summary>
-		/// <param name="id">Id of the verse type</param>
-		/// <returns></returns>
-		Task<bool> VerseTypeExistByIdAsync(int id);
+	/// <summary>
+	/// Returns true if the verse type exists.
+	/// Returns false if there is no such verse type with this id.
+	/// </summary>
+	/// <param name="id">Id of the verse type</param>
+	/// <returns></returns>
+	Task<bool> VerseTypeExistByIdAsync(int id);
 
-		/// <summary>
-		/// Returns true if there is at least one verse choreography that uses this verse type. Otherwise it returns false.
-		/// Throws an exception if there is no verse type with this id.
-		/// </summary>
-		/// <param name="id">Id of the verse type</param>
-		/// <returns></returns>
-		Task<bool> IsVerseTypeUsedInChoreographiesAsync(int id);
+	/// <summary>
+	/// Returns true if there is at least one verse choreography that uses this verse type. Otherwise it returns false.
+	/// Throws an exception if there is no verse type with this id.
+	/// </summary>
+	/// <param name="id">Id of the verse type</param>
+	/// <returns></returns>
+	Task<bool> IsVerseTypeUsedInChoreographiesAsync(int id);
 
-		/// <summary>
-		/// Adds a new verse type with data from the model.
-		/// Throws an exception if the given beats count in the model are not an even number.
-		/// </summary>
-		/// <param name="model">VerseTypeFormViewModel model</param>
-		/// <returns></returns>
-		Task AddVerseTypeAsync(VerseTypeFormViewModel model);
+	/// <summary>
+	/// Adds a new verse type with data from the model.
+	/// Throws an exception if the given beats count in the model are not an even number.
+	/// </summary>
+	/// <param name="model">VerseTypeFormViewModel model</param>
+	/// <returns></returns>
+	Task AddVerseTypeAsync(VerseTypeFormViewModel model);
 
-		/// <summary>
-		/// Changes the status of the verse type with this id.
-		/// Throws an exception if there is no verse type with this id.
-		/// </summary>
-		/// <param name="id">Id of the verse type</param>
-		/// <returns></returns>
-		Task ChangeVerseTypeStatusAsync(int id);
+	/// <summary>
+	/// Changes the status of the verse type with this id.
+	/// Throws an exception if there is no verse type with this id.
+	/// </summary>
+	/// <param name="id">Id of the verse type</param>
+	/// <returns></returns>
+	Task ChangeVerseTypeStatusAsync(int id);
 
-		/// <summary>
-		/// Edits the verse type with the given id with the updated data from the model.
-		/// Throws an exception if there is no such verse type with this id.
-		/// Throws an exception if the given beats count in the model are not an even number.
-		/// </summary>
-		/// <param name="verseTypeId">Id of the verse type</param>
-		/// <param name="model">VerseTypeFormViewModel model</param>
-		/// <returns></returns>
-		Task EditVerseTypeAsync(int verseTypeId, VerseTypeFormViewModel model);
+	/// <summary>
+	/// Edits the verse type with the given id with the updated data from the model.
+	/// Throws an exception if there is no such verse type with this id.
+	/// Throws an exception if the given beats count in the model are not an even number.
+	/// </summary>
+	/// <param name="verseTypeId">Id of the verse type</param>
+	/// <param name="model">VerseTypeFormViewModel model</param>
+	/// <returns></returns>
+	Task EditVerseTypeAsync(int verseTypeId, VerseTypeFormViewModel model);
 
-		/// <summary>
-		/// Deletes a verse type by its id.
-		/// </summary>
-		/// <param name="id">Id of the verse type</param>
-		/// <returns></returns>
-		Task DeleteVerseTypeAsync(int id);
-	}
+	/// <summary>
+	/// Deletes a verse type by its id.
+	/// </summary>
+	/// <param name="id">Id of the verse type</param>
+	/// <returns></returns>
+	Task DeleteVerseTypeAsync(int id);
 }
