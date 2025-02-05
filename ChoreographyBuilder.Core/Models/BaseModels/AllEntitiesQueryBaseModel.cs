a4ea17ed5ba52a3d2ebf0c7bb7c900be9da911bf
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-05 09:03:24 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/BaseModels/AllEntitiesQueryBaseModel.cs
+++ ChoreographyBuilder.Core/Models/BaseModels/AllEntitiesQueryBaseModel.cs
@@ -1,24 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.LimitConstants;
 
-namespace ChoreographyBuilder.Core.Models.BaseModels
+namespace ChoreographyBuilder.Core.Models.BaseModels;
+
+/// <summary>
+/// A base class for all models used to get filtering criteria and pagination info.
+/// No added validation attributes.
+/// </summary>
+/// <typeparam name="Т">Type of model entities</typeparam>
+public abstract class AllEntitiesQueryBaseModel<Т>
 {
-	/// <summary>
-	/// A base class for all models used to get filtering criteria and pagination info.
-	/// No added validation attributes.
-	/// </summary>
-	/// <typeparam name="Т">Type of model entities</typeparam>
-	public abstract class AllEntitiesQueryBaseModel<Т>
-	{
-		[Display(Name = "Search by name")]
-		public string? SearchTerm { get; init; }
+	[Display(Name = "Search by name")]
+	public string? SearchTerm { get; init; }
 
-		public int ItemsPerPage { get; init; } = DefaultNumberOfItemsPerPage;
+	public int ItemsPerPage { get; init; } = DefaultNumberOfItemsPerPage;
 
-		public int CurrentPage { get; init; } = 1;
+	public int CurrentPage { get; init; } = 1;
 
-		public int TotalItemCount { get; set; }
+	public int TotalItemCount { get; set; }
 
-		public IEnumerable<Т> Entities { get; set; } = new List<Т>();
-	}
+	public IEnumerable<Т> Entities { get; set; } = new List<Т>();
 }
