ae1cbda4177a0f75a893a7637de6c5ae9edf7172
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-05 09:03:31 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/Figure/AllFiguresQueryModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/AllFiguresQueryModel.cs
@@ -3,28 +3,27 @@ using ChoreographyBuilder.Core.Models.Position;
 using ChoreographyBuilder.Infrastructure.Data.Models.Enums;
 using System.ComponentModel.DataAnnotations;
 
-namespace ChoreographyBuilder.Core.Models.Figure
+namespace ChoreographyBuilder.Core.Models.Figure;
+
+/// <summary>
+/// A model used for getting criteria for filtering figures and pagination info.
+/// No added validation attributes.
+/// </summary>
+public class AllFiguresQueryModel : AllEntitiesQueryBaseModel <FigureTableViewModel>
 {
-	/// <summary>
-	/// A model used for getting criteria for filtering figures and pagination info.
-	/// No added validation attributes.
-	/// </summary>
-	public class AllFiguresQueryModel : AllEntitiesQueryBaseModel <FigureTableViewModel>
-	{
-		[Display(Name = "Option start position")]
-		public int? StartPosition { get; init; }
+	[Display(Name = "Option start position")]
+	public int? StartPosition { get; init; }
 
-		[Display(Name = "Option end position")]
-		public int? EndPosition { get; init; }
+	[Display(Name = "Option end position")]
+	public int? EndPosition { get; init; }
 
-		[Display(Name = "Opion beats count")]
-		public int? BeatsCount { get; init; }
+	[Display(Name = "Opion beats count")]
+	public int? BeatsCount { get; init; }
 
-		[Display(Name = "Option dynamics type")]
-		public DynamicsType? DynamicsType { get; init; }
+	[Display(Name = "Option dynamics type")]
+	public DynamicsType? DynamicsType { get; init; }
 
-		public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
+	public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
 
-		public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
-	}
+	public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
 }
