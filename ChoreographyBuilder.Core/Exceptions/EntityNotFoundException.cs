d3611d2a46dd37a15b6c6e7fcde50b723f219bcf
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-05 09:03:07 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Exceptions/EntityNotFoundException.cs
+++ ChoreographyBuilder.Core/Exceptions/EntityNotFoundException.cs
@@ -1,11 +1,10 @@
-﻿namespace ChoreographyBuilder.Core.Exceptions
+﻿namespace ChoreographyBuilder.Core.Exceptions;
+
+public class EntityNotFoundException : Exception
 {
-	public class EntityNotFoundException : Exception
-	{
-		private const string DefaultMessage = "This entity could not be found!";
+	private const string DefaultMessage = "This entity could not be found!";
 
-		public EntityNotFoundException() : base(DefaultMessage) { }
+	public EntityNotFoundException() : base(DefaultMessage) { }
 
-		public EntityNotFoundException(string message) : base(message) { }
-	}
+	public EntityNotFoundException(string message) : base(message) { }
 }
--- ChoreographyBuilder.Core/Exceptions/InvalidModelException.cs
+++ ChoreographyBuilder.Core/Exceptions/InvalidModelException.cs
@@ -1,11 +1,10 @@
-﻿namespace ChoreographyBuilder.Core.Exceptions
+﻿namespace ChoreographyBuilder.Core.Exceptions;
+
+public class InvalidModelException : Exception
 {
-	public class InvalidModelException : Exception
-	{
-		private const string DefaultMessage = "The given model is invalid!";
+	private const string DefaultMessage = "The given model is invalid!";
 
-		public InvalidModelException() : base(DefaultMessage) { }
+	public InvalidModelException() : base(DefaultMessage) { }
 
-		public InvalidModelException(string message) : base(message) { }
-	}
+	public InvalidModelException(string message) : base(message) { }
 }
