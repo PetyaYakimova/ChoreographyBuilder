fb81c1a2fb7e602d566ab70c9a2a762320a83d43
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-06 09:04:24 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/Figure/FigureQueryServiceModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/FigureQueryServiceModel.cs
@@ -1,12 +1,11 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
 
-namespace ChoreographyBuilder.Core.Models.Figure
+namespace ChoreographyBuilder.Core.Models.Figure;
+
+/// <summary>
+/// A model that has the total count of figures and a collection of a certain amount of them to display them on pages.
+/// No added validation attributes.
+/// </summary>
+public class FigureQueryServiceModel : EntityQueryBaseModel<FigureTableViewModel>
 {
-	/// <summary>
-	/// A model that has the total count of figures and a collection of a certain amount of them to display them on pages.
-	/// No added validation attributes.
-	/// </summary>
-	public class FigureQueryServiceModel : EntityQueryBaseModel<FigureTableViewModel>
-	{
-	}
 }
--- ChoreographyBuilder.Core/Models/Figure/FigureTableViewModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/FigureTableViewModel.cs
@@ -1,25 +1,24 @@
-﻿namespace ChoreographyBuilder.Core.Models.Figure
+﻿namespace ChoreographyBuilder.Core.Models.Figure;
+
+/// <summary>
+/// View model only for previewing figures in a table. 
+/// No added validation attributes.
+/// </summary>
+public class FigureTableViewModel
 {
-	/// <summary>
-	/// View model only for previewing figures in a table. 
-	/// No added validation attributes.
-	/// </summary>
-	public class FigureTableViewModel
-	{
-		public int Id { get; set; }
+	public int Id { get; set; }
 
-		public string Name { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
 
-		public bool IsHighlight { get; set; }
+	public bool IsHighlight { get; set; }
 
-		public bool IsFavourite { get; set; }
+	public bool IsFavourite { get; set; }
 
-		public bool CanBeShared { get; set; }
+	public bool CanBeShared { get; set; }
 
-		public int FigureOptionsCount { get; set; }
+	public int FigureOptionsCount { get; set; }
 
-		public bool FigureUsedInChoreographies { get; set; }
+	public bool FigureUsedInChoreographies { get; set; }
 
-		public string UserEmailAddress { get; set; } = string.Empty;
-	}
+	public string UserEmailAddress { get; set; } = string.Empty;
 }
