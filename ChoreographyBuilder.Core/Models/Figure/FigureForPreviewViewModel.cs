d5f6da04295dd8929c80a50e20eef70bfb021b87
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-06 09:04:16 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/Figure/FigureForPreviewViewModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/FigureForPreviewViewModel.cs
@@ -1,13 +1,12 @@
-﻿namespace ChoreographyBuilder.Core.Models.Figure
-{
-    /// <summary>
-	/// View model only for previewing figures when filtering verse choreographies and when trying to delete figure. 
-    /// No added validation attributes.
-	/// </summary>
-    public class FigureForPreviewViewModel
-    {
-        public int Id { get; set; }
+﻿namespace ChoreographyBuilder.Core.Models.Figure;
 
-        public string Name { get; set; } = string.Empty;
-    }
+/// <summary>
+	/// View model only for previewing figures when filtering verse choreographies and when trying to delete figure. 
+/// No added validation attributes.
+	/// </summary>
+public class FigureForPreviewViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
 }
