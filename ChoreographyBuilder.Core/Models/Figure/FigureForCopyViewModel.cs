29cb108f3d88010590219133f6a8d8966542392f
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-06 09:04:06 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/Figure/FigureForCopyViewModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/FigureForCopyViewModel.cs
@@ -1,13 +1,12 @@
-﻿namespace ChoreographyBuilder.Core.Models.Figure
-{
-	/// <summary>
-	/// View model only for previewing figures when copying a figure. 
-	/// No added validation attributes.
-	/// </summary>
-	public class FigureForCopyViewModel : FigureForPreviewViewModel
-	{
-		public int FigureOptionsCount { get; set; }
+﻿namespace ChoreographyBuilder.Core.Models.Figure;
 
-		public string UserEmailAddress { get; set; } = null!;
-	}
+/// <summary>
+/// View model only for previewing figures when copying a figure. 
+/// No added validation attributes.
+/// </summary>
+public class FigureForCopyViewModel : FigureForPreviewViewModel
+{
+	public int FigureOptionsCount { get; set; }
+
+	public string UserEmailAddress { get; set; } = null!;
 }
--- ChoreographyBuilder.Core/Models/Figure/FigureFormViewModel.cs
+++ ChoreographyBuilder.Core/Models/Figure/FigureFormViewModel.cs
@@ -3,30 +3,29 @@ using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Core.Models.Figure
+namespace ChoreographyBuilder.Core.Models.Figure;
+
+/// <summary>
+/// View model for creating and editing figures. 
+/// Added validation attributes.
+/// </summary>
+public class FigureFormViewModel
 {
-	/// <summary>
-	/// View model for creating and editing figures. 
-	/// Added validation attributes.
-	/// </summary>
-	public class FigureFormViewModel
-	{
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[StringLength(FigureNameMaxLength,
-			MinimumLength = FigureNameMinLength,
-			ErrorMessage = StringLengthErrorMessage)]
-		public string Name { get; set; } = string.Empty;
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[StringLength(FigureNameMaxLength,
+		MinimumLength = FigureNameMinLength,
+		ErrorMessage = StringLengthErrorMessage)]
+	public string Name { get; set; } = string.Empty;
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[DisplayName("Highlight Figure")]
-		public bool IsHighlight { get; set; } = false;
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[DisplayName("Highlight Figure")]
+	public bool IsHighlight { get; set; } = false;
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[DisplayName("Favourite Figure")]
-		public bool IsFavourite { get; set; } = false;
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[DisplayName("Favourite Figure")]
+	public bool IsFavourite { get; set; } = false;
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[DisplayName("Share Figure With Other Users")]
-		public bool CanBeShared { get; set; } = false;
-	}
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[DisplayName("Share Figure With Other Users")]
+	public bool CanBeShared { get; set; } = false;
 }
