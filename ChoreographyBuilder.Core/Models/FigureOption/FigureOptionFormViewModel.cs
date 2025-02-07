408de2b869113fcfde2acdf281a5ea3389546581
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-07 08:47:33 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/FigureOption/FigureOptionFormViewModel.cs
+++ ChoreographyBuilder.Core/Models/FigureOption/FigureOptionFormViewModel.cs
@@ -6,14 +6,14 @@ using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Core.Models.FigureOption
+namespace ChoreographyBuilder.Core.Models.FigureOption;
+
+/// <summary>
+/// View model for creating and editing option for figure. 
+/// Added validation attributes.
+/// </summary>
+public class FigureOptionFormViewModel
 {
-	/// <summary>
-	/// View model for creating and editing option for figure. 
-	/// Added validation attributes.
-	/// </summary>
-	public class FigureOptionFormViewModel
-	{
         [Required(ErrorMessage = RequiredErrorMessage)]
         public int FigureId { get; set; }
 
@@ -22,29 +22,28 @@ namespace ChoreographyBuilder.Core.Models.FigureOption
         public string FigureName { get; set;} = string.Empty;
 
         [Required(ErrorMessage = RequiredErrorMessage)]
-		[DisplayName("Start Position")]
-		public int StartPositionId { get; set; }
+	[DisplayName("Start Position")]
+	public int StartPositionId { get; set; }
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[DisplayName("End Position")]
-		public int EndPositionId { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[DisplayName("End Position")]
+	public int EndPositionId { get; set; }
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[Range(FigureOptionBeatsCountMin,
-			FigureOptionBeatsCountMax,
-			ErrorMessage = NumberMustBeInRangeErrorMessage)]
-		[IsEven(ErrorMessage = NumberMustBeEvenErrorMessage)]
-		[Display(Name = "Beats Count")]
-		public int BeatCounts { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[Range(FigureOptionBeatsCountMin,
+		FigureOptionBeatsCountMax,
+		ErrorMessage = NumberMustBeInRangeErrorMessage)]
+	[IsEven(ErrorMessage = NumberMustBeEvenErrorMessage)]
+	[Display(Name = "Beats Count")]
+	public int BeatCounts { get; set; }
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[DisplayName("Dynamics Type")]
-		public DynamicsType DynamicsType { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[DisplayName("Dynamics Type")]
+	public DynamicsType DynamicsType { get; set; }
 
-		public IEnumerable<PositionForPreviewViewModel> StartPositions { get; set; } = new List<PositionForPreviewViewModel>();
+	public IEnumerable<PositionForPreviewViewModel> StartPositions { get; set; } = new List<PositionForPreviewViewModel>();
 
-		public IEnumerable<PositionForPreviewViewModel> EndPositions { get; set; } = new List<PositionForPreviewViewModel>();
+	public IEnumerable<PositionForPreviewViewModel> EndPositions { get; set; } = new List<PositionForPreviewViewModel>();
 
-		public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
-	}
+	public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
 }
--- ChoreographyBuilder.Core/Models/FigureOption/FigureOptionQueryServiceModel.cs
+++ ChoreographyBuilder.Core/Models/FigureOption/FigureOptionQueryServiceModel.cs
@@ -1,15 +1,14 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
 
-namespace ChoreographyBuilder.Core.Models.FigureOption
-{
-	/// <summary>
-	/// A model that has the total count of figure options and a collection of a certain amount of them to display them on pages.
-	/// No added validation attributes.
-	/// </summary>
-	public class FigureOptionQueryServiceModel : EntityQueryBaseModel<FigureOptionTableViewModel>
-	{
-		public int FigureId { get; set; }
+namespace ChoreographyBuilder.Core.Models.FigureOption;
 
-		public string FigureName { get; set; } = string.Empty;
-	}
+/// <summary>
+/// A model that has the total count of figure options and a collection of a certain amount of them to display them on pages.
+/// No added validation attributes.
+/// </summary>
+public class FigureOptionQueryServiceModel : EntityQueryBaseModel<FigureOptionTableViewModel>
+{
+	public int FigureId { get; set; }
+
+	public string FigureName { get; set; } = string.Empty;
 }
