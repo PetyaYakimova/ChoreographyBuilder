b0d3acee7fd0a3fa890d2972af758c7941b71714
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-07 08:47:25 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/FigureOption/AllFigureOptionsQueryModel.cs
+++ ChoreographyBuilder.Core/Models/FigureOption/AllFigureOptionsQueryModel.cs
@@ -3,31 +3,30 @@ using ChoreographyBuilder.Core.Models.Position;
 using ChoreographyBuilder.Infrastructure.Data.Models.Enums;
 using System.ComponentModel.DataAnnotations;
 
-namespace ChoreographyBuilder.Core.Models.FigureOption
+namespace ChoreographyBuilder.Core.Models.FigureOption;
+
+/// <summary>
+/// A model used for getting criteria for filtering figure options and pagination info.
+/// No added validation attributes.
+/// </summary>
+public class AllFigureOptionsQueryModel : AllEntitiesQueryBaseModel<FigureOptionTableViewModel>
 {
-	/// <summary>
-	/// A model used for getting criteria for filtering figure options and pagination info.
-	/// No added validation attributes.
-	/// </summary>
-	public class AllFigureOptionsQueryModel : AllEntitiesQueryBaseModel<FigureOptionTableViewModel>
-	{
-		public int? FigureId { get; set; }
-		public string FigureName { get; set; } = string.Empty;
+	public int? FigureId { get; set; }
+	public string FigureName { get; set; } = string.Empty;
 
-		[Display(Name = "Start position")]
-		public int? StartPosition { get; init; }
+	[Display(Name = "Start position")]
+	public int? StartPosition { get; init; }
 
-		[Display(Name = "End position")]
-		public int? EndPosition { get; init; }
+	[Display(Name = "End position")]
+	public int? EndPosition { get; init; }
 
-		[Display(Name = "Beats count")]
-		public int? BeatsCount { get; init; }
+	[Display(Name = "Beats count")]
+	public int? BeatsCount { get; init; }
 
-		[Display(Name = "Dynamics type")]
-		public DynamicsType? DynamicsType { get; init; }
+	[Display(Name = "Dynamics type")]
+	public DynamicsType? DynamicsType { get; init; }
 
-		public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
+	public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
 
-		public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
-	}
+	public IEnumerable<DynamicsType> DynamicsTypes { get; set; } = new List<DynamicsType>();
 }
--- ChoreographyBuilder.Core/Models/FigureOption/FigureOptionDeleteViewModel.cs
+++ ChoreographyBuilder.Core/Models/FigureOption/FigureOptionDeleteViewModel.cs
@@ -1,15 +1,14 @@
-﻿namespace ChoreographyBuilder.Core.Models.FigureOption
+﻿namespace ChoreographyBuilder.Core.Models.FigureOption;
+
+/// <summary>
+/// View model for previewing figure option when trying to delete it. 
+/// No added validation attributes.
+/// </summary>
+public class FigureOptionDeleteViewModel
 {
-	/// <summary>
-	/// View model for previewing figure option when trying to delete it. 
-	/// No added validation attributes.
-	/// </summary>
-	public class FigureOptionDeleteViewModel
-	{
-		public int Id { get; set; }
+	public int Id { get; set; }
 
-		public string FigureName { get; set; } = string.Empty;
+	public string FigureName { get; set; } = string.Empty;
 
-		public int FigureId { get; set; }
-	}
+	public int FigureId { get; set; }
 }
