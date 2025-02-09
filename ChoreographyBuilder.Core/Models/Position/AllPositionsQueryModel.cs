5256355c96def4e7b4d1357d0bcce3dffe244b10
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-09 08:53:54 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/Position/AllPositionsQueryModel.cs
+++ ChoreographyBuilder.Core/Models/Position/AllPositionsQueryModel.cs
@@ -1,12 +1,11 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
 
-namespace ChoreographyBuilder.Core.Models.Position
+namespace ChoreographyBuilder.Core.Models.Position;
+
+/// <summary>
+/// A model used for getting criteria for filtering positions and pagination info.
+/// No added validation attributes.
+/// </summary>
+public class AllPositionsQueryModel : AllEntitiesQueryBaseModel<PositionTableViewModel>
 {
-	/// <summary>
-	/// A model used for getting criteria for filtering positions and pagination info.
-	/// No added validation attributes.
-	/// </summary>
-	public class AllPositionsQueryModel : AllEntitiesQueryBaseModel<PositionTableViewModel>
-	{
-	}
 }
--- ChoreographyBuilder.Core/Models/Position/PositionFormViewModel.cs
+++ ChoreographyBuilder.Core/Models/Position/PositionFormViewModel.cs
@@ -2,21 +2,20 @@
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Core.Models.Position
-{
-	/// <summary>
-	/// View model for creating and editing positions. 
-	/// Added validation attributes.
-	/// </summary>
-	public class PositionFormViewModel
-	{
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[StringLength(PositionNameMaxLength,
-			MinimumLength = PositionNameMinLength,
-			ErrorMessage = StringLengthErrorMessage)]
-		public string Name { get; set; } = string.Empty;
+namespace ChoreographyBuilder.Core.Models.Position;
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		public bool IsActive { get; set; } = true;
-	}
+/// <summary>
+/// View model for creating and editing positions. 
+/// Added validation attributes.
+/// </summary>
+public class PositionFormViewModel
+{
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[StringLength(PositionNameMaxLength,
+		MinimumLength = PositionNameMinLength,
+		ErrorMessage = StringLengthErrorMessage)]
+	public string Name { get; set; } = string.Empty;
+
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	public bool IsActive { get; set; } = true;
 }
