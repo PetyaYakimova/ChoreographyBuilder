580e3404f661e7c98305ccf9400cf9a399d954a0
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-10 08:51:42 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseChoreography/AllVerseChoreographiesQueryModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreography/AllVerseChoreographiesQueryModel.cs
@@ -4,33 +4,32 @@ using ChoreographyBuilder.Core.Models.Position;
 using ChoreographyBuilder.Core.Models.VerseType;
 using System.ComponentModel.DataAnnotations;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreography
-{
+namespace ChoreographyBuilder.Core.Models.VerseChoreography;
+
 	/// <summary>
 	/// A model used for getting criteria for filtering verse choreographies and pagination info.
 	/// No added validation attributes.
 	/// </summary>
 	public class AllVerseChoreographiesQueryModel : AllEntitiesQueryBaseModel<VerseChoreographyTableViewModel>
-    {
-        [Display(Name = "Search by verse type")]
-        public int? VerseType { get; init; }
+{
+    [Display(Name = "Search by verse type")]
+    public int? VerseType { get; init; }
 
-        [Display(Name = "Search by start position")]
-        public int? StartPosition { get; init; }
+    [Display(Name = "Search by start position")]
+    public int? StartPosition { get; init; }
 
-        [Display(Name = "Search by end position")]
-        public int? EndPosition { get; init; }
+    [Display(Name = "Search by end position")]
+    public int? EndPosition { get; init; }
 
-        [Display(Name = "Search by beats")]
-        public int? BeatsCount { get; init; }
+    [Display(Name = "Search by beats")]
+    public int? BeatsCount { get; init; }
 
-        [Display(Name = "Search by final figure")]
-        public int? FinalFigure { get; init; }
+    [Display(Name = "Search by final figure")]
+    public int? FinalFigure { get; init; }
 
-        public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
+    public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
 
-        public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
+    public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
 
-        public IEnumerable<FigureForPreviewViewModel> Figures { get; set; } = new List<FigureForPreviewViewModel>();
-    }
+    public IEnumerable<FigureForPreviewViewModel> Figures { get; set; } = new List<FigureForPreviewViewModel>();
 }
