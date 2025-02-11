f428dfbc6a03351c21e58c56bc93d8f3e84d8fce
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-11 08:44:12 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyDetailsViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyDetailsViewModel.cs
@@ -1,13 +1,12 @@
 ﻿using ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreography
+namespace ChoreographyBuilder.Core.Models.VerseChoreography;
+
+/// <summary>
+/// View model only for previewing details of saved verse choreographies. 
+/// No added validation attributes.
+/// </summary>
+public class VerseChoreographyDetailsViewModel : VerseChoreographyTableViewModel
 {
-	/// <summary>
-	/// View model only for previewing details of saved verse choreographies. 
-	/// No added validation attributes.
-	/// </summary>
-	public class VerseChoreographyDetailsViewModel : VerseChoreographyTableViewModel
-	{
-		public IList<VerseChoreographyFigureViewModel> Figures { get; set; } = new List<VerseChoreographyFigureViewModel>();
-	}
+	public IList<VerseChoreographyFigureViewModel> Figures { get; set; } = new List<VerseChoreographyFigureViewModel>();
 }
--- ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyFormViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyFormViewModel.cs
@@ -3,23 +3,22 @@ using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreography
+namespace ChoreographyBuilder.Core.Models.VerseChoreography;
+
+/// <summary>
+/// A model for generating by hand a verse choreography. 
+/// Added validation attributes.
+/// </summary>
+public class VerseChoreographyFormViewModel
 {
-	/// <summary>
-	/// A model for generating by hand a verse choreography. 
-	/// Added validation attributes.
-	/// </summary>
-	public class VerseChoreographyFormViewModel
-	{
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[StringLength(VerseChoreographyNameMaxLength,
-			MinimumLength = VerseChoreographyNameMinLength,
-			ErrorMessage = StringLengthErrorMessage)]
-		public string Name { get; set; } = string.Empty;
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[StringLength(VerseChoreographyNameMaxLength,
+		MinimumLength = VerseChoreographyNameMinLength,
+		ErrorMessage = StringLengthErrorMessage)]
+	public string Name { get; set; } = string.Empty;
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		public int VerseTypeId { get; init; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	public int VerseTypeId { get; init; }
 
-		public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
-	}
+	public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
 }
