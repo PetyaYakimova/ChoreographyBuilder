06ac448d952c2b0fc8d8154ad3e520b2579853f2
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-11 08:44:33 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyGenerateModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyGenerateModel.cs
@@ -4,29 +4,28 @@ using ChoreographyBuilder.Core.Models.VerseType;
 using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreography
+namespace ChoreographyBuilder.Core.Models.VerseChoreography;
+
+/// <summary>
+/// A model for getting the requirements before generating suggestions for verse choreographies.
+/// Added validation attributes.
+/// </summary>
+public class VerseChoreographyGenerateModel
 {
-	/// <summary>
-	/// A model for getting the requirements before generating suggestions for verse choreographies.
-	/// Added validation attributes.
-	/// </summary>
-	public class VerseChoreographyGenerateModel
-	{
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[Display(Name = "Verse type")]
-		public int VerseTypeId { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[Display(Name = "Verse type")]
+	public int VerseTypeId { get; set; }
 
-		[Display(Name = "Start position (Optional)")]
-		public int? StartPositionId { get; set; }
+	[Display(Name = "Start position (Optional)")]
+	public int? StartPositionId { get; set; }
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[Display(Name = "Final figure")]
-		public int FinalFigureId { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[Display(Name = "Final figure")]
+	public int FinalFigureId { get; set; }
 
-		public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
+	public IEnumerable<VerseTypeForPreviewViewModel> VerseTypes { get; set; } = new List<VerseTypeForPreviewViewModel>();
 
-		public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
+	public IEnumerable<PositionForPreviewViewModel> Positions { get; set; } = new List<PositionForPreviewViewModel>();
 
-		public IEnumerable<FigureForPreviewViewModel> Figures { get; set; } = new List<FigureForPreviewViewModel>();
-	}
+	public IEnumerable<FigureForPreviewViewModel> Figures { get; set; } = new List<FigureForPreviewViewModel>();
 }
--- ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyQueryServiceModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreography/VerseChoreographyQueryServiceModel.cs
@@ -1,12 +1,11 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreography
-{
-	/// <summary>
-	/// A model that has the total count of verse choreographies and a collection of a certain amount of them to display them on pages.
-	/// No added validation attributes.
-	/// </summary>
-	public class VerseChoreographyQueryServiceModel : EntityQueryBaseModel<VerseChoreographyTableViewModel>
+namespace ChoreographyBuilder.Core.Models.VerseChoreography;
+
+/// <summary>
+/// A model that has the total count of verse choreographies and a collection of a certain amount of them to display them on pages.
+/// No added validation attributes.
+/// </summary>
+public class VerseChoreographyQueryServiceModel : EntityQueryBaseModel<VerseChoreographyTableViewModel>
     {
     }
-}
