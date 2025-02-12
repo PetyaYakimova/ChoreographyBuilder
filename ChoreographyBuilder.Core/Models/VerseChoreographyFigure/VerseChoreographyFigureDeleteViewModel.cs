85c3bd0354eae20ea4e5b001304f2c044f69cd96
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-12 08:55:52 +0200

    Update namespace

--- ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureDeleteViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureDeleteViewModel.cs
@@ -1,17 +1,16 @@
-﻿namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
-{
-    /// <summary>
+﻿namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
+
+/// <summary>
 	/// View model for previewing figure in verse choreography when trying to delete it. 
 	/// No added validation attributes.
 	/// </summary>
-    public class VerseChoreographyFigureDeleteViewModel
-    {
-        public int Id { get; set; }
+public class VerseChoreographyFigureDeleteViewModel
+{
+    public int Id { get; set; }
 
-        public string FigureName { get; set; } = string.Empty;
+    public string FigureName { get; set; } = string.Empty;
 
-        public string VerseChoreographyName { get; set; } = string.Empty;
+    public string VerseChoreographyName { get; set; } = string.Empty;
 
-        public int VerseChoreographyId { get; set; }
-    }
+    public int VerseChoreographyId { get; set; }
 }
