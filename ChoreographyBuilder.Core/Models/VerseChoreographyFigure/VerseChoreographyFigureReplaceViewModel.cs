d7504b384acfc0eae6b675958a3872147bf03d3c
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-13 08:32:58 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureReplaceViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureReplaceViewModel.cs
@@ -1,18 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
-{
-	/// <summary>
-	/// View model for previewing a figure in verse choreography when replacing it. 
-	/// Added validation attributes.
-	/// </summary>
-	public class VerseChoreographyFigureReplaceViewModel : VerseChoreographyFigureViewModel
-	{
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[Display(Name = "New figure:")]
-		public int ReplacementFigureOptionId { get; set; }
+namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
 
-		public IEnumerable<VerseChoreographyFigureViewModel> PossibleReplacementFigures { get; set; } = new List<VerseChoreographyFigureViewModel>();
-	}
+/// <summary>
+/// View model for previewing a figure in verse choreography when replacing it. 
+/// Added validation attributes.
+/// </summary>
+public class VerseChoreographyFigureReplaceViewModel : VerseChoreographyFigureViewModel
+{
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[Display(Name = "New figure:")]
+	public int ReplacementFigureOptionId { get; set; }
+
+	public IEnumerable<VerseChoreographyFigureViewModel> PossibleReplacementFigures { get; set; } = new List<VerseChoreographyFigureViewModel>();
 }
