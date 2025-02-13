94cb31c82a0bbe273ff75836059f6b3bd5fd5507
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-13 08:33:15 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseType/AllVerseTypesQueryModel.cs
+++ ChoreographyBuilder.Core/Models/VerseType/AllVerseTypesQueryModel.cs
@@ -1,15 +1,14 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
 using System.ComponentModel.DataAnnotations;
 
-namespace ChoreographyBuilder.Core.Models.VerseType
+namespace ChoreographyBuilder.Core.Models.VerseType;
+
+/// <summary>
+/// A model used for getting criteria for filtering verse types and pagination info.
+/// No added validation attributes.
+/// </summary>
+public class AllVerseTypesQueryModel : AllEntitiesQueryBaseModel<VerseTypeTableViewModel>
 {
-	/// <summary>
-	/// A model used for getting criteria for filtering verse types and pagination info.
-	/// No added validation attributes.
-	/// </summary>
-	public class AllVerseTypesQueryModel : AllEntitiesQueryBaseModel<VerseTypeTableViewModel>
-	{
-		[Display(Name = "Search by beats")]
-		public int? SearchBeats { get; init; }
-	}
+	[Display(Name = "Search by beats")]
+	public int? SearchBeats { get; init; }
 }
