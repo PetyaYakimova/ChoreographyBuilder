ecdbd142cf909633486b2fe317b153a684dfd9d8
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-13 08:33:07 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureSelectedReplacementServiceModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureSelectedReplacementServiceModel.cs
@@ -1,13 +1,12 @@
-﻿namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
-{
-	/// <summary>
-	/// Service model for transferring the data for the selected new figure in the verse choreography.
-	/// No added validation attributes.
-	/// </summary>
-	public class VerseChoreographyFigureSelectedReplacementServiceModel
-	{
-		public int FigureOptionId { get; set; }
+﻿namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
 
-		public int FigureOrder { get; set; }
-	}
+/// <summary>
+/// Service model for transferring the data for the selected new figure in the verse choreography.
+/// No added validation attributes.
+/// </summary>
+public class VerseChoreographyFigureSelectedReplacementServiceModel
+{
+	public int FigureOptionId { get; set; }
+
+	public int FigureOrder { get; set; }
 }
--- ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureViewModel.cs
@@ -1,41 +1,40 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
+namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
+
+/// <summary>
+/// View model only for previewing the figures in a verse choreography 
+/// Added validation attribute only for the figure order property.
+/// </summary>
+public class VerseChoreographyFigureViewModel
 {
-	/// <summary>
-	/// View model only for previewing the figures in a verse choreography 
-	/// Added validation attribute only for the figure order property.
-	/// </summary>
-	public class VerseChoreographyFigureViewModel
-	{
-		public int Id { get; set; }
+	public int Id { get; set; }
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[Display(Name = "Figure order in choreography:")]
-		public int FigureOrder { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[Display(Name = "Figure order in choreography:")]
+	public int FigureOrder { get; set; }
 
-		public int FigureOptionId { get; set; }
+	public int FigureOptionId { get; set; }
 
-		[Display(Name = "Figure name:")]
-		public string FigureName { get; init; } = string.Empty;
+	[Display(Name = "Figure name:")]
+	public string FigureName { get; init; } = string.Empty;
 
-		[Display(Name = "Is favourite:")]
-		public bool IsFavourite { get; init; }
+	[Display(Name = "Is favourite:")]
+	public bool IsFavourite { get; init; }
 
-		[Display(Name = "Is highlight:")]
-		public bool IsHighlight { get; init; }
+	[Display(Name = "Is highlight:")]
+	public bool IsHighlight { get; init; }
 
-		[Display(Name = "Start position:")]
-		public string StartPosition { get; init; } = string.Empty;
+	[Display(Name = "Start position:")]
+	public string StartPosition { get; init; } = string.Empty;
 
-		[Display(Name = "End position:")]
-		public string EndPosition { get; init; } = string.Empty;
+	[Display(Name = "End position:")]
+	public string EndPosition { get; init; } = string.Empty;
 
-		[Display(Name = "Beats count:")]
-		public int BeatsCount { get; init; }
+	[Display(Name = "Beats count:")]
+	public int BeatsCount { get; init; }
 
-		[Display(Name = "Dynamics type:")]
-		public string DynamicsType { get; init; } = string.Empty;
-	}
+	[Display(Name = "Dynamics type:")]
+	public string DynamicsType { get; init; } = string.Empty;
 }
