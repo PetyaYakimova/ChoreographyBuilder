9e353c4c6ea90b8dac02d08ca84a9d981d526603
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-13 08:32:44 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureOptionFormViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseChoreographyFigure/VerseChoreographyFigureOptionFormViewModel.cs
@@ -2,28 +2,27 @@
 using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 
-namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure
-{
-    /// <summary>
+namespace ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
+
+/// <summary>
 	/// View model only for when selecting figure option for verse choreography. 
 	/// Added validation attributes.
 	/// </summary>
 	public class VerseChoreographyFigureOptionFormViewModel
-    {
-        [Required(ErrorMessage = RequiredErrorMessage)]
-        [Display(Name = "Figure option:")]
-        public int FigureOptionId { get; set; }
+{
+    [Required(ErrorMessage = RequiredErrorMessage)]
+    [Display(Name = "Figure option:")]
+    public int FigureOptionId { get; set; }
 
-        [Required(ErrorMessage = RequiredErrorMessage)]
-        [Display(Name = "Order:")]
-        public int FigureOrder { get; set; }
+    [Required(ErrorMessage = RequiredErrorMessage)]
+    [Display(Name = "Order:")]
+    public int FigureOrder { get; set; }
 
-        [Display(Name = "Start position:")]
-        public string? StartPositionName { get; set; }
+    [Display(Name = "Start position:")]
+    public string? StartPositionName { get; set; }
 
-        [Display(Name = "Remaining beats in the choreography:")]
-        public int RemainingBeats { get; set; }
+    [Display(Name = "Remaining beats in the choreography:")]
+    public int RemainingBeats { get; set; }
 
-        public IEnumerable<FigureOptionWithFigureViewModel> Figures { get; set; } = new List<FigureOptionWithFigureViewModel>();
-    }
+    public IEnumerable<FigureOptionWithFigureViewModel> Figures { get; set; } = new List<FigureOptionWithFigureViewModel>();
 }
