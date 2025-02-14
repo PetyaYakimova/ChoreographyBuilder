1ed19cba418611b7788b0fbd2f0ac73b48375064
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-14 09:14:27 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseType/VerseTypeForPreviewViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseType/VerseTypeForPreviewViewModel.cs
@@ -1,13 +1,12 @@
-﻿namespace ChoreographyBuilder.Core.Models.VerseType
-{
-	/// <summary>
-	/// View model only for previewing verse types when selecting one for a choreography, when filling dropdowns and when previewing it before delete. 
-	/// No added validation attributes.
-	/// </summary>
-	public class VerseTypeForPreviewViewModel
-	{
-		public int Id { get; set; }
+﻿namespace ChoreographyBuilder.Core.Models.VerseType;
 
-		public string Name { get; set; } = string.Empty;
-	}
+/// <summary>
+/// View model only for previewing verse types when selecting one for a choreography, when filling dropdowns and when previewing it before delete. 
+/// No added validation attributes.
+/// </summary>
+public class VerseTypeForPreviewViewModel
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; } = string.Empty;
 }
--- ChoreographyBuilder.Core/Models/VerseType/VerseTypeFormViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseType/VerseTypeFormViewModel.cs
@@ -3,29 +3,28 @@ using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Core.Models.VerseType
+namespace ChoreographyBuilder.Core.Models.VerseType;
+
+/// <summary>
+/// View model for creating and editing verse types. 
+/// Added validation attributes.
+/// </summary>
+public class VerseTypeFormViewModel
 {
-	/// <summary>
-	/// View model for creating and editing verse types. 
-	/// Added validation attributes.
-	/// </summary>
-	public class VerseTypeFormViewModel
-	{
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[StringLength(VerseTypeNameMaxLength,
-			MinimumLength = VerseTypeNameMinLength,
-			ErrorMessage = StringLengthErrorMessage)]
-		public string Name { get; set; } = string.Empty;
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[StringLength(VerseTypeNameMaxLength,
+		MinimumLength = VerseTypeNameMinLength,
+		ErrorMessage = StringLengthErrorMessage)]
+	public string Name { get; set; } = string.Empty;
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		[Range(VerseTypeBeatsCountMin,
-			VerseTypeBeatsCountMax,
-			ErrorMessage = NumberMustBeInRangeErrorMessage)]
-		[IsEven(ErrorMessage = NumberMustBeEvenErrorMessage)]
-		[Display(Name = "Beats Count")]
-		public int BeatCounts { get; set; }
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	[Range(VerseTypeBeatsCountMin,
+		VerseTypeBeatsCountMax,
+		ErrorMessage = NumberMustBeInRangeErrorMessage)]
+	[IsEven(ErrorMessage = NumberMustBeEvenErrorMessage)]
+	[Display(Name = "Beats Count")]
+	public int BeatCounts { get; set; }
 
-		[Required(ErrorMessage = RequiredErrorMessage)]
-		public bool IsActive { get; set; } = true;
-	}
+	[Required(ErrorMessage = RequiredErrorMessage)]
+	public bool IsActive { get; set; } = true;
 }
