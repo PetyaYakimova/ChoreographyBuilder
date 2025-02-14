7c6ba7a15678da02375e4d04731c527c7058f57c
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-14 09:14:47 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Models/VerseType/VerseTypeQueryServiceModel.cs
+++ ChoreographyBuilder.Core/Models/VerseType/VerseTypeQueryServiceModel.cs
@@ -1,12 +1,11 @@
 ﻿using ChoreographyBuilder.Core.Models.BaseModels;
 
-namespace ChoreographyBuilder.Core.Models.VerseType
+namespace ChoreographyBuilder.Core.Models.VerseType;
+
+/// <summary>
+/// A model that has the total count of verse types and a collection of a certain amount of them to display them on pages.
+/// No added validation attributes.
+/// </summary>
+public class VerseTypeQueryServiceModel : EntityQueryBaseModel<VerseTypeTableViewModel>
 {
-	/// <summary>
-	/// A model that has the total count of verse types and a collection of a certain amount of them to display them on pages.
-	/// No added validation attributes.
-	/// </summary>
-	public class VerseTypeQueryServiceModel : EntityQueryBaseModel<VerseTypeTableViewModel>
-	{
-	}
 }
--- ChoreographyBuilder.Core/Models/VerseType/VerseTypeTableViewModel.cs
+++ ChoreographyBuilder.Core/Models/VerseType/VerseTypeTableViewModel.cs
@@ -1,19 +1,18 @@
-﻿namespace ChoreographyBuilder.Core.Models.VerseType
+﻿namespace ChoreographyBuilder.Core.Models.VerseType;
+
+/// <summary>
+/// View model only for previewing verse types in a table. 
+/// No added validation attributes.
+/// </summary>
+public class VerseTypeTableViewModel
 {
-	/// <summary>
-	/// View model only for previewing verse types in a table. 
-	/// No added validation attributes.
-	/// </summary>
-	public class VerseTypeTableViewModel
-	{
-		public int Id { get; set; }
+	public int Id { get; set; }
 
-		public string Name { get; set; } = string.Empty;
+	public string Name { get; set; } = string.Empty;
 
-		public int BeatCounts { get; set; }
+	public int BeatCounts { get; set; }
 
-		public bool IsActive { get; set; }
+	public bool IsActive { get; set; }
 
-		public bool HasChoreographies { get; set; }
-	}
+	public bool HasChoreographies { get; set; }
 }
