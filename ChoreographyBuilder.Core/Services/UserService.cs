42567dd304cf85ef3ef766e17d2f263ab2aa0b96
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-16 23:02:37 +0200

    Update namespaces

--- ChoreographyBuilder.Core/Services/UserService.cs
+++ ChoreographyBuilder.Core/Services/UserService.cs
@@ -7,117 +7,116 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using static ChoreographyBuilder.Core.Constants.LimitConstants;
 
-namespace ChoreographyBuilder.Core.Services
+namespace ChoreographyBuilder.Core.Services;
+
+public class UserService : IUserService
 {
-	public class UserService : IUserService
+	private readonly IRepository repository;
+	public UserService(IRepository repository)
 	{
-		private readonly IRepository repository;
-		public UserService(IRepository repository)
+		this.repository = repository;
+	}
+
+	public async Task<AdminStatisticModel> GetAdminStatisticsAsync()
+	{
+		AdminStatisticModel model = new AdminStatisticModel();
+
+		model.TotalNumberOfPositions = await repository.AllAsReadOnly<Position>()
+			.CountAsync();
+
+		model.NumberOfActivePositions = await repository.AllAsReadOnly<Position>()
+			.Where(p => p.IsActive)
+			.CountAsync();
+
+		model.TotalNumberOfVerseTypes = await repository.AllAsReadOnly<VerseType>()
+			.CountAsync();
+
+		model.NumberOfActiveVerseTypes = await repository.AllAsReadOnly<VerseType>()
+			.Where(vt => vt.IsActive)
+			.CountAsync();
+
+		model.TotalNumberOfFigures = await repository.AllAsReadOnly<Figure>()
+			.CountAsync();
+
+		model.UsersWithAtLeastOneFigure = await repository.AllAsReadOnly<Figure>()
+			.Select(f => f.UserId)
+			.Distinct()
+			.CountAsync();
+
+		model.TotalNumberOfSavedVerseChoreographies = await repository.AllAsReadOnly<VerseChoreography>()
+			.CountAsync();
+
+		model.UsersWithAtLeastOneVerseChoreography = await repository.AllAsReadOnly<VerseChoreography>()
+			.Select(f => f.UserId)
+			.Distinct()
+			.CountAsync();
+
+		model.TotalNumberOfSavedFullChoreographies = await repository.AllAsReadOnly<FullChoreography>()
+			.CountAsync();
+
+		model.UsersWithAtLeastOneFullChoreography = await repository.AllAsReadOnly<FullChoreography>()
+			.Select(f => f.UserId)
+			.Distinct()
+			.CountAsync();
+
+		return model;
+	}
+
+	public async Task<UserQueryServiceModel> GetAllUserStatisticsAsync(string? searchTerm = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage)
+	{
+		var usersToShow = repository.AllAsReadOnly<IdentityUser>();
+
+		if (searchTerm != null)
 		{
-			this.repository = repository;
+			string normalizedSearchTerm = searchTerm.ToLower();
+			usersToShow = usersToShow
+				.Where(p => p.Email.ToLower().Contains(normalizedSearchTerm));
 		}
 
-		public async Task<AdminStatisticModel> GetAdminStatisticsAsync()
-		{
-			AdminStatisticModel model = new AdminStatisticModel();
-
-			model.TotalNumberOfPositions = await repository.AllAsReadOnly<Position>()
-				.CountAsync();
-
-			model.NumberOfActivePositions = await repository.AllAsReadOnly<Position>()
-				.Where(p => p.IsActive)
-				.CountAsync();
-
-			model.TotalNumberOfVerseTypes = await repository.AllAsReadOnly<VerseType>()
-				.CountAsync();
-
-			model.NumberOfActiveVerseTypes = await repository.AllAsReadOnly<VerseType>()
-				.Where(vt => vt.IsActive)
-				.CountAsync();
-
-			model.TotalNumberOfFigures = await repository.AllAsReadOnly<Figure>()
-				.CountAsync();
-
-			model.UsersWithAtLeastOneFigure = await repository.AllAsReadOnly<Figure>()
-				.Select(f => f.UserId)
-				.Distinct()
-				.CountAsync();
-
-			model.TotalNumberOfSavedVerseChoreographies = await repository.AllAsReadOnly<VerseChoreography>()
-				.CountAsync();
-
-			model.UsersWithAtLeastOneVerseChoreography = await repository.AllAsReadOnly<VerseChoreography>()
-				.Select(f => f.UserId)
-				.Distinct()
-				.CountAsync();
-
-			model.TotalNumberOfSavedFullChoreographies = await repository.AllAsReadOnly<FullChoreography>()
-				.CountAsync();
-
-			model.UsersWithAtLeastOneFullChoreography = await repository.AllAsReadOnly<FullChoreography>()
-				.Select(f => f.UserId)
-				.Distinct()
-				.CountAsync();
-
-			return model;
-		}
-
-		public async Task<UserQueryServiceModel> GetAllUserStatisticsAsync(string? searchTerm = null, int currentPage = 1, int itemsPerPage = DefaultNumberOfItemsPerPage)
-		{
-			var usersToShow = repository.AllAsReadOnly<IdentityUser>();
-
-			if (searchTerm != null)
+		var users = await usersToShow
+			.OrderBy(u => u.Id)
+			.Skip((currentPage - 1) * itemsPerPage)
+			.Take(itemsPerPage)
+			.Select(u => new UserTableViewModel()
 			{
-				string normalizedSearchTerm = searchTerm.ToLower();
-				usersToShow = usersToShow
-					.Where(p => p.Email.ToLower().Contains(normalizedSearchTerm));
-			}
+				Id = u.Id,
+				Email = u.Email
+			})
+			.ToListAsync();
 
-			var users = await usersToShow
-				.OrderBy(u => u.Id)
-				.Skip((currentPage - 1) * itemsPerPage)
-				.Take(itemsPerPage)
-				.Select(u => new UserTableViewModel()
-				{
-					Id = u.Id,
-					Email = u.Email
-				})
-				.ToListAsync();
-
-			foreach (var user in users)
-			{
-				UserStatisticModel statistics = await this.GetUserStatisticsAsync(user.Id);
-				user.MyTotalNumberOfFigures = statistics.MyTotalNumberOfFigures;
-				user.MyTotalNumberOfFullChoreographies = statistics.MyTotalNumberOfFullChoreographies;
-				user.MyTotalNumberOfVerseChoreographies = statistics.MyTotalNumberOfVerseChoreographies;
-			}
-
-			int totalUsersToShow = await usersToShow.CountAsync();
-
-			return new UserQueryServiceModel()
-			{
-				TotalCount = totalUsersToShow,
-				Entities = users
-			};
-		}
-
-		public async Task<UserStatisticModel> GetUserStatisticsAsync(string userId)
+		foreach (var user in users)
 		{
-			UserStatisticModel model = new UserStatisticModel();
-
-			model.MyTotalNumberOfFigures = await repository.AllAsReadOnly<Figure>()
-				.Where(f => f.UserId == userId)
-				.CountAsync();
-
-			model.MyTotalNumberOfVerseChoreographies = await repository.AllAsReadOnly<VerseChoreography>()
-				.Where(c => c.UserId == userId)
-				.CountAsync();
-
-			model.MyTotalNumberOfFullChoreographies = await repository.AllAsReadOnly<FullChoreography>()
-				.Where(c => c.UserId == userId)
-				.CountAsync();
-
-			return model;
+			UserStatisticModel statistics = await this.GetUserStatisticsAsync(user.Id);
+			user.MyTotalNumberOfFigures = statistics.MyTotalNumberOfFigures;
+			user.MyTotalNumberOfFullChoreographies = statistics.MyTotalNumberOfFullChoreographies;
+			user.MyTotalNumberOfVerseChoreographies = statistics.MyTotalNumberOfVerseChoreographies;
 		}
+
+		int totalUsersToShow = await usersToShow.CountAsync();
+
+		return new UserQueryServiceModel()
+		{
+			TotalCount = totalUsersToShow,
+			Entities = users
+		};
+	}
+
+	public async Task<UserStatisticModel> GetUserStatisticsAsync(string userId)
+	{
+		UserStatisticModel model = new UserStatisticModel();
+
+		model.MyTotalNumberOfFigures = await repository.AllAsReadOnly<Figure>()
+			.Where(f => f.UserId == userId)
+			.CountAsync();
+
+		model.MyTotalNumberOfVerseChoreographies = await repository.AllAsReadOnly<VerseChoreography>()
+			.Where(c => c.UserId == userId)
+			.CountAsync();
+
+		model.MyTotalNumberOfFullChoreographies = await repository.AllAsReadOnly<FullChoreography>()
+			.Where(c => c.UserId == userId)
+			.CountAsync();
+
+		return model;
 	}
 }
--- ChoreographyBuilder.Core/Services/VerseChoreographyFigureService.cs
+++ ChoreographyBuilder.Core/Services/VerseChoreographyFigureService.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using ChoreographyBuilder.Core.Contracts;
 using ChoreographyBuilder.Core.Exceptions;
-using ChoreographyBuilder.Core.Models.FullChoreographyVerseChoreography;
 using ChoreographyBuilder.Core.Models.VerseChoreographyFigure;
 using ChoreographyBuilder.Infrastructure.Data.Common;
 using ChoreographyBuilder.Infrastructure.Data.Models;
@@ -9,176 +8,175 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using static ChoreographyBuilder.Core.Constants.MessageConstants;
 
-namespace ChoreographyBuilder.Core.Services
+namespace ChoreographyBuilder.Core.Services;
+
+public class VerseChoreographyFigureService : IVerseChoreographyFigureService
 {
-    public class VerseChoreographyFigureService : IVerseChoreographyFigureService
+    private readonly ILogger<VerseChoreographyFigureService> logger;
+    private readonly IRepository repository;
+    private readonly IMapper mapper;
+
+    public VerseChoreographyFigureService(ILogger<VerseChoreographyFigureService> logger, IRepository repository, IMapper mapper)
     {
-        private readonly ILogger<VerseChoreographyFigureService> logger;
-        private readonly IRepository repository;
-        private readonly IMapper mapper;
+        this.logger = logger;
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
-        public VerseChoreographyFigureService(ILogger<VerseChoreographyFigureService> logger, IRepository repository, IMapper mapper)
+    public async Task<VerseChoreographyFigureReplaceViewModel> GetVerseChoreographyFigureForReplaceAsync(int verseChoreographyFigureId)
+    {
+        var option = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+            .Include(vcf => vcf.FigureOption)
+                .ThenInclude(fo => fo.Figure)
+            .Include(vcf => vcf.FigureOption)
+                .ThenInclude(fo => fo.StartPosition)
+            .Include(vcf => vcf.FigureOption)
+                .ThenInclude(fo => fo.EndPosition)
+            .FirstOrDefaultAsync(fo => fo.Id == verseChoreographyFigureId);
+
+        if (option == null)
         {
-            this.logger = logger;
-            this.repository = repository;
-            this.mapper = mapper;
+            logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+            throw new EntityNotFoundException();
         }
 
-        public async Task<VerseChoreographyFigureReplaceViewModel> GetVerseChoreographyFigureForReplaceAsync(int verseChoreographyFigureId)
+        return mapper.Map<VerseChoreographyFigureReplaceViewModel>(option);
+    }
+
+    public async Task<IEnumerable<VerseChoreographyFigureViewModel>> GetPossibleReplacementsForVerseChoreographyFigureAsync(int verseChoreographyFigureId)
+    {
+        var currentFigure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+            .Include(vcf => vcf.VerseChoreography)
+            .Include(vcf => vcf.FigureOption)
+            .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
+
+        if (currentFigure == null)
         {
-            var option = await repository.AllAsReadOnly<VerseChoreographyFigure>()
-                .Include(vcf => vcf.FigureOption)
-                    .ThenInclude(fo => fo.Figure)
-                .Include(vcf => vcf.FigureOption)
-                    .ThenInclude(fo => fo.StartPosition)
-                .Include(vcf => vcf.FigureOption)
-                    .ThenInclude(fo => fo.EndPosition)
-                .FirstOrDefaultAsync(fo => fo.Id == verseChoreographyFigureId);
-
-            if (option == null)
-            {
-                logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
-                throw new EntityNotFoundException();
-            }
-
-            return mapper.Map<VerseChoreographyFigureReplaceViewModel>(option);
+            logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+            throw new EntityNotFoundException();
         }
 
-        public async Task<IEnumerable<VerseChoreographyFigureViewModel>> GetPossibleReplacementsForVerseChoreographyFigureAsync(int verseChoreographyFigureId)
+        var result = await repository.AllAsReadOnly<FigureOption>()
+            .Where(fo => fo.Figure.UserId == currentFigure.VerseChoreography.UserId)
+            .Where(fo => fo.FigureId != currentFigure.FigureOption.FigureId)
+            .Where(fo => fo.StartPositionId == currentFigure.FigureOption.StartPositionId)
+            .Where(fo => fo.EndPositionId == currentFigure.FigureOption.EndPositionId)
+            .Where(fo => fo.BeatCounts == currentFigure.FigureOption.BeatCounts)
+            .Include(f => f.Figure)
+            .Include(f => f.StartPosition)
+            .Include(f => f.EndPosition)
+            .ToListAsync();
+
+        return mapper.Map<List<VerseChoreographyFigureViewModel>>(result);
+    }
+
+    public async Task<VerseChoreographyFigureDeleteViewModel> GetFigureForDeleteAsync(int verseChoreographyFigureId)
+    {
+        var figure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+            .Include(vcf => vcf.VerseChoreography)
+            .Include(vcf => vcf.FigureOption.Figure)
+            .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
+
+        if (figure == null)
         {
-            var currentFigure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
-                .Include(vcf => vcf.VerseChoreography)
-                .Include(vcf => vcf.FigureOption)
-                .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
-
-            if (currentFigure == null)
-            {
-                logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
-                throw new EntityNotFoundException();
-            }
-
-            var result = await repository.AllAsReadOnly<FigureOption>()
-                .Where(fo => fo.Figure.UserId == currentFigure.VerseChoreography.UserId)
-                .Where(fo => fo.FigureId != currentFigure.FigureOption.FigureId)
-                .Where(fo => fo.StartPositionId == currentFigure.FigureOption.StartPositionId)
-                .Where(fo => fo.EndPositionId == currentFigure.FigureOption.EndPositionId)
-                .Where(fo => fo.BeatCounts == currentFigure.FigureOption.BeatCounts)
-                .Include(f => f.Figure)
-                .Include(f => f.StartPosition)
-                .Include(f => f.EndPosition)
-                .ToListAsync();
-
-            return mapper.Map<List<VerseChoreographyFigureViewModel>>(result);
+            logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+            throw new EntityNotFoundException();
         }
 
-        public async Task<VerseChoreographyFigureDeleteViewModel> GetFigureForDeleteAsync(int verseChoreographyFigureId)
+        return mapper.Map<VerseChoreographyFigureDeleteViewModel>(figure);
+    }
+
+    public async Task<int> GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(int verseChoreographyFigureId)
+    {
+        var verseChoreographyFigure = await repository.GetByIdAsync<VerseChoreographyFigure>(verseChoreographyFigureId);
+
+        if (verseChoreographyFigure == null)
         {
-            var figure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
-                .Include(vcf => vcf.VerseChoreography)
-                .Include(vcf => vcf.FigureOption.Figure)
-                .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
-
-            if (figure == null)
-            {
-                logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
-                throw new EntityNotFoundException();
-            }
-
-            return mapper.Map<VerseChoreographyFigureDeleteViewModel>(figure);
+            logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
+            throw new EntityNotFoundException();
         }
 
-        public async Task<int> GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(int verseChoreographyFigureId)
+        return verseChoreographyFigure.VerseChoreographyId;
+    }
+
+    public async Task<bool> VerseChoreographyFigureExistForThisUserByIdAsync(int verseChoreographyFigureId, string userId)
+    {
+        VerseChoreographyFigure? verseChoreographyFigure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+            .Include(vcf => vcf.VerseChoreography)
+            .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
+
+        if (verseChoreographyFigure == null)
         {
-            var verseChoreographyFigure = await repository.GetByIdAsync<VerseChoreographyFigure>(verseChoreographyFigureId);
-
-            if (verseChoreographyFigure == null)
-            {
-                logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreographyFigure), verseChoreographyFigureId);
-                throw new EntityNotFoundException();
-            }
-
-            return verseChoreographyFigure.VerseChoreographyId;
+            return false;
         }
 
-        public async Task<bool> VerseChoreographyFigureExistForThisUserByIdAsync(int verseChoreographyFigureId, string userId)
+        if (verseChoreographyFigure.VerseChoreography.UserId != userId)
         {
-            VerseChoreographyFigure? verseChoreographyFigure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
-                .Include(vcf => vcf.VerseChoreography)
-                .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
-
-            if (verseChoreographyFigure == null)
-            {
-                return false;
-            }
-
-            if (verseChoreographyFigure.VerseChoreography.UserId != userId)
-            {
-                return false;
-            }
-
-            return true;
+            return false;
         }
 
-        public async Task<bool> FigureIsLastForVerseChoreographyByIdAsync(int verseChoreographyFigureId)
+        return true;
+    }
+
+    public async Task<bool> FigureIsLastForVerseChoreographyByIdAsync(int verseChoreographyFigureId)
+    {
+        VerseChoreographyFigure? figure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+            .Include(vcf => vcf.VerseChoreography)
+            .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
+
+        if (figure == null)
         {
-            VerseChoreographyFigure? figure = await repository.AllAsReadOnly<VerseChoreographyFigure>()
-                .Include(vcf => vcf.VerseChoreography)
-                .FirstOrDefaultAsync(vcf => vcf.Id == verseChoreographyFigureId);
-
-            if (figure == null)
-            {
-                return false;
-            }
-
-            VerseChoreographyFigure? lastfigureForTheVerseChoreography = await repository.AllAsReadOnly<VerseChoreographyFigure>()
-                .Where(vcf => vcf.VerseChoreographyId == figure.VerseChoreographyId)
-                .OrderByDescending(vcf => vcf.FigureOrder)
-                .FirstOrDefaultAsync();
-
-            if (lastfigureForTheVerseChoreography == null || lastfigureForTheVerseChoreography.Id != verseChoreographyFigureId)
-            {
-                return false;
-            }
-
-            return true;
+            return false;
         }
 
-        public async Task AddFigureToVerseChoreographyAsync(int verseChoreographyId, VerseChoreographyFigureOptionFormViewModel model)
+        VerseChoreographyFigure? lastfigureForTheVerseChoreography = await repository.AllAsReadOnly<VerseChoreographyFigure>()
+            .Where(vcf => vcf.VerseChoreographyId == figure.VerseChoreographyId)
+            .OrderByDescending(vcf => vcf.FigureOrder)
+            .FirstOrDefaultAsync();
+
+        if (lastfigureForTheVerseChoreography == null || lastfigureForTheVerseChoreography.Id != verseChoreographyFigureId)
         {
-            var verseChoreography = await repository.GetByIdAsync<VerseChoreography>(verseChoreographyId);
-            if (verseChoreography == null)
-            {
-                logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreography), verseChoreographyId);
-                throw new EntityNotFoundException();
-            }
-
-            var figureOption = await repository.AllAsReadOnly<FigureOption>()
-                .Include(fo => fo.Figure)
-                .FirstOrDefaultAsync(fo => fo.Id == model.FigureOptionId);
-            if (figureOption == null)
-            {
-                logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(FigureOption), model.FigureOptionId);
-                throw new EntityNotFoundException();
-            }
-
-            if (verseChoreography.UserId != figureOption.Figure.UserId)
-            {
-                logger.LogError(UserForTheFigureAndForTheVerseChoreographyIsNotTheSameLoggerErrorMessage);
-                throw new InvalidModelException(UserForTheFigureAndForTheVerseChoreographyIsNotTheSameLoggerErrorMessage);
-            }
-
-            VerseChoreographyFigure entity = mapper.Map<VerseChoreographyFigure>(model);
-            entity.VerseChoreographyId = verseChoreographyId;
-
-            await repository.AddAsync(entity);
-
-            await repository.SaveChangesAsync();
+            return false;
         }
 
-        public async Task DeleteFigureFromVerseChoreographyAsync(int verseChoreographyFigureId)
+        return true;
+    }
+
+    public async Task AddFigureToVerseChoreographyAsync(int verseChoreographyId, VerseChoreographyFigureOptionFormViewModel model)
+    {
+        var verseChoreography = await repository.GetByIdAsync<VerseChoreography>(verseChoreographyId);
+        if (verseChoreography == null)
         {
-            await repository.DeleteAsync<VerseChoreographyFigure>(verseChoreographyFigureId);
-            await repository.SaveChangesAsync();
+            logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(VerseChoreography), verseChoreographyId);
+            throw new EntityNotFoundException();
         }
+
+        var figureOption = await repository.AllAsReadOnly<FigureOption>()
+            .Include(fo => fo.Figure)
+            .FirstOrDefaultAsync(fo => fo.Id == model.FigureOptionId);
+        if (figureOption == null)
+        {
+            logger.LogError(EntityWithIdWasNotFoundLoggerErrorMessage, nameof(FigureOption), model.FigureOptionId);
+            throw new EntityNotFoundException();
+        }
+
+        if (verseChoreography.UserId != figureOption.Figure.UserId)
+        {
+            logger.LogError(UserForTheFigureAndForTheVerseChoreographyIsNotTheSameLoggerErrorMessage);
+            throw new InvalidModelException(UserForTheFigureAndForTheVerseChoreographyIsNotTheSameLoggerErrorMessage);
+        }
+
+        VerseChoreographyFigure entity = mapper.Map<VerseChoreographyFigure>(model);
+        entity.VerseChoreographyId = verseChoreographyId;
+
+        await repository.AddAsync(entity);
+
+        await repository.SaveChangesAsync();
+    }
+
+    public async Task DeleteFigureFromVerseChoreographyAsync(int verseChoreographyFigureId)
+    {
+        await repository.DeleteAsync<VerseChoreographyFigure>(verseChoreographyFigureId);
+        await repository.SaveChangesAsync();
     }
 }
