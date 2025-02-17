c05596a4a7eec7e3e4d81f3a01a2af35a40d5213
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-17 08:40:18 +0200

    Update namespace

--- ChoreographyBuilder.Infrastructure/Data/Models/Figure.cs
+++ ChoreographyBuilder.Infrastructure/Data/Models/Figure.cs
@@ -4,39 +4,38 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Infrastructure.Data.Models
+namespace ChoreographyBuilder.Infrastructure.Data.Models;
+
+[Comment("Figures")]
+public class Figure
 {
-	[Comment("Figures")]
-	public class Figure
-	{
-		[Key]
-		[Comment("Figure Identifier")]
-		public int Id { get; set; }
+	[Key]
+	[Comment("Figure Identifier")]
+	public int Id { get; set; }
 
-		[Required]
-		[MaxLength(FigureNameMaxLength)]
-		[Comment("Figure Name")]
-		public string Name { get; set; } = string.Empty;
+	[Required]
+	[MaxLength(FigureNameMaxLength)]
+	[Comment("Figure Name")]
+	public string Name { get; set; } = string.Empty;
 
-		[Required]
-		[Comment("Figure Is Highlight")]
-		public bool IsHighlight { get; set; }
+	[Required]
+	[Comment("Figure Is Highlight")]
+	public bool IsHighlight { get; set; }
 
-		[Required]
-		[Comment("Figure Is Favourite")]
-		public bool IsFavourite { get; set; }
+	[Required]
+	[Comment("Figure Is Favourite")]
+	public bool IsFavourite { get; set; }
 
-		[Required]
-		[Comment("User Identifier")]
-		public string UserId { get; set; } = string.Empty;
+	[Required]
+	[Comment("User Identifier")]
+	public string UserId { get; set; } = string.Empty;
 
-		[ForeignKey(nameof(UserId))]
-		public IdentityUser User { get; set; } = null!;
+	[ForeignKey(nameof(UserId))]
+	public IdentityUser User { get; set; } = null!;
 
-		[Required]
-		[Comment("Figure Can Be Shared With Other Users")]
-		public bool CanBeShared { get; set; }
+	[Required]
+	[Comment("Figure Can Be Shared With Other Users")]
+	public bool CanBeShared { get; set; }
 
-		public IEnumerable<FigureOption> FigureOptions { get; set; } = new List<FigureOption>();
-	}
+	public IEnumerable<FigureOption> FigureOptions { get; set; } = new List<FigureOption>();
 }
