2f4eeab0b7f9bdb2f4f5cc88935c63d6e160e304
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-18 08:39:06 +0200

    Update namespaces

--- ChoreographyBuilder.Infrastructure/Data/Models/FigureOption.cs
+++ ChoreographyBuilder.Infrastructure/Data/Models/FigureOption.cs
@@ -3,43 +3,42 @@ using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ChoreographyBuilder.Infrastructure.Data.Models
+namespace ChoreographyBuilder.Infrastructure.Data.Models;
+
+[Comment("Figure Options")]
+public class FigureOption
 {
-	[Comment("Figure Options")]
-	public class FigureOption
-	{
-		[Key]
-		[Comment("Figure Options Identifier")]
-		public int Id { get; set; }
+	[Key]
+	[Comment("Figure Options Identifier")]
+	public int Id { get; set; }
 
-		[Required]
-		[Comment("Figure Options Figure Identifier")]
-		public int FigureId { get; set; }
+	[Required]
+	[Comment("Figure Options Figure Identifier")]
+	public int FigureId { get; set; }
 
-		[ForeignKey(nameof(FigureId))]
-		public Figure Figure { get; set; } = null!;
+	[ForeignKey(nameof(FigureId))]
+	public Figure Figure { get; set; } = null!;
 
-		[Required]
-		[Comment("Figure Options Start Position Identifier")]
-		public int StartPositionId { get; set; }
+	[Required]
+	[Comment("Figure Options Start Position Identifier")]
+	public int StartPositionId { get; set; }
 
-		[ForeignKey(nameof(StartPositionId))]
-		public Position StartPosition { get; set; } = null!;
+	[ForeignKey(nameof(StartPositionId))]
+	public Position StartPosition { get; set; } = null!;
 
-		[Required]
-		[Comment("Figure Options End Position Identifier")]
-		public int EndPositionId { get; set; }
+	[Required]
+	[Comment("Figure Options End Position Identifier")]
+	public int EndPositionId { get; set; }
 
-		[ForeignKey(nameof(EndPositionId))]
-		public Position EndPosition { get; set; } = null!;
+	[ForeignKey(nameof(EndPositionId))]
+	public Position EndPosition { get; set; } = null!;
 
-		[Required]
-		[Comment("Figure Option Beat Counts")]
-		public int BeatCounts { get; set; }
+	[Required]
+	[Comment("Figure Option Beat Counts")]
+	public int BeatCounts { get; set; }
 
-		[Required]
-		public DynamicsType DynamicsType { get; set; }
+	[Required]
+	public DynamicsType DynamicsType { get; set; }
 
-		public IEnumerable<VerseChoreographyFigure> VerseChoreographyFigures { get; set; } = new List<VerseChoreographyFigure>();
-	}
+	public IEnumerable<VerseChoreographyFigure> VerseChoreographyFigures { get; set; } = new List<VerseChoreographyFigure>();
 }
--- ChoreographyBuilder.Infrastructure/Data/Models/FullChoreography.cs
+++ ChoreographyBuilder.Infrastructure/Data/Models/FullChoreography.cs
@@ -4,27 +4,26 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Infrastructure.Data.Models
+namespace ChoreographyBuilder.Infrastructure.Data.Models;
+
+[Comment("Full Choreographies")]
+public class FullChoreography
 {
-	[Comment("Full Choreographies")]
-	public class FullChoreography
-	{
-		[Key]
-		[Comment("Full Choreography Identifier")]
-		public int Id { get; set; }
+	[Key]
+	[Comment("Full Choreography Identifier")]
+	public int Id { get; set; }
 
-		[Required]
-		[MaxLength(FullChoreographyNameMaxLength)]
-		[Comment("Full Choreography Name")]
-		public string Name { get; set; } = string.Empty;
+	[Required]
+	[MaxLength(FullChoreographyNameMaxLength)]
+	[Comment("Full Choreography Name")]
+	public string Name { get; set; } = string.Empty;
 
-		[Required]
-		[Comment("User Identifier")]
-		public string UserId { get; set; } = string.Empty;
+	[Required]
+	[Comment("User Identifier")]
+	public string UserId { get; set; } = string.Empty;
 
-		[ForeignKey(nameof(UserId))]
-		public IdentityUser User { get; set; } = null!;
+	[ForeignKey(nameof(UserId))]
+	public IdentityUser User { get; set; } = null!;
 
-		public IEnumerable<FullChoreographyVerseChoreography> VerseChoreographies { get; set; } = new List<FullChoreographyVerseChoreography>();
-	}
+	public IEnumerable<FullChoreographyVerseChoreography> VerseChoreographies { get; set; } = new List<FullChoreographyVerseChoreography>();
 }
