63499107896b2ab6fba3fb38f7d9ad3f275cb029
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-18 08:39:22 +0200

    Update namespaces

--- ChoreographyBuilder.Infrastructure/Data/Models/FullChoreographyVerseChoreography.cs
+++ ChoreographyBuilder.Infrastructure/Data/Models/FullChoreographyVerseChoreography.cs
@@ -2,31 +2,30 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ChoreographyBuilder.Infrastructure.Data.Models
+namespace ChoreographyBuilder.Infrastructure.Data.Models;
+
+[Comment("Full Choreography Verse Choreographies")]
+public class FullChoreographyVerseChoreography
 {
-	[Comment("Full Choreography Verse Choreographies")]
-	public class FullChoreographyVerseChoreography
-	{
-		[Key]
-		[Comment("Full Choreography Verse Choreography Identifier")]
-		public int Id { get; set; }
+	[Key]
+	[Comment("Full Choreography Verse Choreography Identifier")]
+	public int Id { get; set; }
 
-		[Required]
-		[Comment("Full Choreography Identifier")]
-		public int FullChoreographyId { get; set; }
+	[Required]
+	[Comment("Full Choreography Identifier")]
+	public int FullChoreographyId { get; set; }
 
-		[ForeignKey(nameof(FullChoreographyId))]
-		public FullChoreography FullChoreography { get; set; } = null!;
+	[ForeignKey(nameof(FullChoreographyId))]
+	public FullChoreography FullChoreography { get; set; } = null!;
 
-		[Required]
-		[Comment("Verse Choreography Identifier")]
-		public int VerseChoreographyId { get; set; }
+	[Required]
+	[Comment("Verse Choreography Identifier")]
+	public int VerseChoreographyId { get; set; }
 
-		[ForeignKey(nameof(VerseChoreographyId))]
-		public VerseChoreography VerseChoreography { get; set;} = null!;
+	[ForeignKey(nameof(VerseChoreographyId))]
+	public VerseChoreography VerseChoreography { get; set;} = null!;
 
-		[Required]
-		[Comment("Verse Choreography Order in which it appears in this choreography")]
-		public int VerseChoreographyOrder { get; set; }
-	}
+	[Required]
+	[Comment("Verse Choreography Order in which it appears in this choreography")]
+	public int VerseChoreographyOrder { get; set; }
 }
--- ChoreographyBuilder.Infrastructure/Data/Models/Position.cs
+++ ChoreographyBuilder.Infrastructure/Data/Models/Position.cs
@@ -2,26 +2,25 @@
 using System.ComponentModel.DataAnnotations;
 using static ChoreographyBuilder.Infrastructure.Constants.DataConstants;
 
-namespace ChoreographyBuilder.Infrastructure.Data.Models
+namespace ChoreographyBuilder.Infrastructure.Data.Models;
+
+[Comment("Positions")]
+public class Position
 {
-	[Comment("Positions")]
-	public class Position
-	{
-		[Key]
-		[Comment("Position Identifier")]
-		public int Id { get; set; }
+	[Key]
+	[Comment("Position Identifier")]
+	public int Id { get; set; }
 
-		[Required]
-		[MaxLength(PositionNameMaxLength)]
-		[Comment("Position Name")]
-		public string Name { get; set; } = string.Empty;
+	[Required]
+	[MaxLength(PositionNameMaxLength)]
+	[Comment("Position Name")]
+	public string Name { get; set; } = string.Empty;
 
-		[Required]
-		[Comment("Position Is Active")]
-		public bool IsActive { get; set; }
+	[Required]
+	[Comment("Position Is Active")]
+	public bool IsActive { get; set; }
 
-		public IEnumerable<FigureOption> FiguresWithStartPosition { get; set; } = new List<FigureOption>();
+	public IEnumerable<FigureOption> FiguresWithStartPosition { get; set; } = new List<FigureOption>();
 
-		public IEnumerable<FigureOption> FiguresWithEndPosition { get; set; } = new List<FigureOption>();
-	}
+	public IEnumerable<FigureOption> FiguresWithEndPosition { get; set; } = new List<FigureOption>();
 }
