4f01c3cad5b18b2dd330239053375a339392c2d0
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-21 07:50:15 +0200

    Update namespace

--- ChoreographyBuilder.Infrastructure/Data/SeedDb/VerseChoreographyConfiguration.cs
+++ ChoreographyBuilder.Infrastructure/Data/SeedDb/VerseChoreographyConfiguration.cs
@@ -2,25 +2,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace ChoreographyBuilder.Infrastructure.Data.SeedDb
+namespace ChoreographyBuilder.Infrastructure.Data.SeedDb;
+
+internal class VerseChoreographyConfiguration : IEntityTypeConfiguration<VerseChoreography>
 {
-	internal class VerseChoreographyConfiguration : IEntityTypeConfiguration<VerseChoreography>
+	private bool seedData;
+
+	public VerseChoreographyConfiguration(bool seedData = true) : base()
 	{
-		private bool seedData;
+		this.seedData = seedData;
+	}
 
-		public VerseChoreographyConfiguration(bool seedData = true) : base()
+	public void Configure(EntityTypeBuilder<VerseChoreography> builder)
+	{
+		if (seedData)
 		{
-			this.seedData = seedData;
-		}
+			var data = new SeedData();
 
-		public void Configure(EntityTypeBuilder<VerseChoreography> builder)
-		{
-			if (seedData)
-			{
-				var data = new SeedData();
-
-				builder.HasData(new VerseChoreography[] { data.SwingVerseChoreography1, data.SwingVerseChoreography2, data.BluesVerseChoreography });
-			}
+			builder.HasData(new VerseChoreography[] { data.SwingVerseChoreography1, data.SwingVerseChoreography2, data.BluesVerseChoreography });
 		}
 	}
 }
