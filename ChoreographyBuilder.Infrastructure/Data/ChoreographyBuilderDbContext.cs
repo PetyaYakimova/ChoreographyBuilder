e3927d93a247f60bb740dcbe83d9f98a240c4d56
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-22 09:34:22 +0200

    Update namespace

--- ChoreographyBuilder.Infrastructure/Data/ChoreographyBuilderDbContext.cs
+++ ChoreographyBuilder.Infrastructure/Data/ChoreographyBuilderDbContext.cs
@@ -3,60 +3,59 @@ using ChoreographyBuilder.Infrastructure.Data.SeedDb;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ChoreographyBuilder.Infrastructure.Data
+namespace ChoreographyBuilder.Infrastructure.Data;
+
+public class ChoreographyBuilderDbContext : IdentityDbContext
 {
-	public class ChoreographyBuilderDbContext : IdentityDbContext
+	private bool seedData;
+
+	public ChoreographyBuilderDbContext(
+		DbContextOptions<ChoreographyBuilderDbContext> options,
+		bool seedData = true)
+		: base(options)
 	{
-		private bool seedData;
-
-		public ChoreographyBuilderDbContext(
-			DbContextOptions<ChoreographyBuilderDbContext> options,
-			bool seedData = true)
-			: base(options)
+		if (Database.IsRelational())
 		{
-			if (Database.IsRelational())
-			{
-				Database.Migrate();
-			}
-			else
-			{
-				Database.EnsureCreated();
-			}
-
-			this.seedData = seedData;
+			Database.Migrate();
+		}
+		else
+		{
+			Database.EnsureCreated();
 		}
 
-		protected override void OnModelCreating(ModelBuilder builder)
-		{
-			builder.ApplyConfiguration(new RoleConfiguration(seedData));
-			builder.ApplyConfiguration(new UserConfiguration(seedData));
-			builder.ApplyConfiguration(new UserInRoleConfiguration(seedData));
-			builder.ApplyConfiguration(new PositionConfiguration(seedData));
-			builder.ApplyConfiguration(new VerseTypeConfiguration(seedData));
-			builder.ApplyConfiguration(new FigureConfiguration(seedData));
-			builder.ApplyConfiguration(new FigureOptionConfiguration(seedData));
-			builder.ApplyConfiguration(new VerseChoreographyConfiguration(seedData));
-			builder.ApplyConfiguration(new VerseChoreographyFigureConfiguration(seedData));
-			builder.ApplyConfiguration(new FullChoreographyConfiguration(seedData));
-			builder.ApplyConfiguration(new FullChoreographyVerseChoreographyConfiguration(seedData));
-
-			base.OnModelCreating(builder);
-		}
-
-		public DbSet<VerseType> VerseTypes { get; set; } = null!;
-
-		public DbSet<Position> Positions { get; set; } = null!;
-
-		public DbSet<Figure> Figures { get; set; } = null!;
-
-		public DbSet<FigureOption> FigureOptions { get; set; } = null!;
-
-		public DbSet<VerseChoreography> VerseChoreographies { get; set; } = null!;
-
-		public DbSet<VerseChoreographyFigure> VerseChoreographiesFigures { get; set; } = null!;
-
-		public DbSet<FullChoreography> FullChoreographies { get; set; } = null!;
-
-		public DbSet<FullChoreographyVerseChoreography> FullChoreographiesVerseChoreographies { get; set; } = null!;
+		this.seedData = seedData;
 	}
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfiguration(new RoleConfiguration(seedData));
+		builder.ApplyConfiguration(new UserConfiguration(seedData));
+		builder.ApplyConfiguration(new UserInRoleConfiguration(seedData));
+		builder.ApplyConfiguration(new PositionConfiguration(seedData));
+		builder.ApplyConfiguration(new VerseTypeConfiguration(seedData));
+		builder.ApplyConfiguration(new FigureConfiguration(seedData));
+		builder.ApplyConfiguration(new FigureOptionConfiguration(seedData));
+		builder.ApplyConfiguration(new VerseChoreographyConfiguration(seedData));
+		builder.ApplyConfiguration(new VerseChoreographyFigureConfiguration(seedData));
+		builder.ApplyConfiguration(new FullChoreographyConfiguration(seedData));
+		builder.ApplyConfiguration(new FullChoreographyVerseChoreographyConfiguration(seedData));
+
+		base.OnModelCreating(builder);
+	}
+
+	public DbSet<VerseType> VerseTypes { get; set; } = null!;
+
+	public DbSet<Position> Positions { get; set; } = null!;
+
+	public DbSet<Figure> Figures { get; set; } = null!;
+
+	public DbSet<FigureOption> FigureOptions { get; set; } = null!;
+
+	public DbSet<VerseChoreography> VerseChoreographies { get; set; } = null!;
+
+	public DbSet<VerseChoreographyFigure> VerseChoreographiesFigures { get; set; } = null!;
+
+	public DbSet<FullChoreography> FullChoreographies { get; set; } = null!;
+
+	public DbSet<FullChoreographyVerseChoreography> FullChoreographiesVerseChoreographies { get; set; } = null!;
 }
