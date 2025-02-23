26ad1ad183dbdc286c428bd900c2937c91957c0d
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-23 10:09:37 +0200

    Update namespaces

--- ChoreographyBuilder.Infrastructure/Migrations/20240227072804_AddDynamicsType.cs
+++ ChoreographyBuilder.Infrastructure/Migrations/20240227072804_AddDynamicsType.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace ChoreographyBuilder.Infrastructure.Migrations
-{
-    public partial class AddDynamicsType : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "DynamicsType",
-                table: "FigureOptions",
-                type: "int",
-                nullable: false,
-                defaultValue: 0);
-        }
+namespace ChoreographyBuilder.Infrastructure.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "DynamicsType",
-                table: "FigureOptions");
-        }
+public partial class AddDynamicsType : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AddColumn<int>(
+            name: "DynamicsType",
+            table: "FigureOptions",
+            type: "int",
+            nullable: false,
+            defaultValue: 0);
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "DynamicsType",
+            table: "FigureOptions");
     }
 }
--- ChoreographyBuilder.Infrastructure/Migrations/20240227084719_ChangeRelationBetweenTables.cs
+++ ChoreographyBuilder.Infrastructure/Migrations/20240227084719_ChangeRelationBetweenTables.cs
@@ -2,80 +2,79 @@
 
 #nullable disable
 
-namespace ChoreographyBuilder.Infrastructure.Migrations
+namespace ChoreographyBuilder.Infrastructure.Migrations;
+
+public partial class ChangeRelationBetweenTables : Migration
 {
-    public partial class ChangeRelationBetweenTables : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_VerseChoreographiesFigures_Figures_FigureId",
-                table: "VerseChoreographiesFigures");
+        migrationBuilder.DropForeignKey(
+            name: "FK_VerseChoreographiesFigures_Figures_FigureId",
+            table: "VerseChoreographiesFigures");
 
-            migrationBuilder.DropIndex(
-                name: "IX_VerseChoreographiesFigures_FigureId",
-                table: "VerseChoreographiesFigures");
+        migrationBuilder.DropIndex(
+            name: "IX_VerseChoreographiesFigures_FigureId",
+            table: "VerseChoreographiesFigures");
 
-            migrationBuilder.DropColumn(
-                name: "FigureId",
-                table: "VerseChoreographiesFigures");
+        migrationBuilder.DropColumn(
+            name: "FigureId",
+            table: "VerseChoreographiesFigures");
 
-            migrationBuilder.AddColumn<int>(
-                name: "FigureOptionId",
-                table: "VerseChoreographiesFigures",
-                type: "int",
-                nullable: false,
-                defaultValue: 0,
-                comment: "Figure Option Identifier");
+        migrationBuilder.AddColumn<int>(
+            name: "FigureOptionId",
+            table: "VerseChoreographiesFigures",
+            type: "int",
+            nullable: false,
+            defaultValue: 0,
+            comment: "Figure Option Identifier");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_VerseChoreographiesFigures_FigureOptionId",
-                table: "VerseChoreographiesFigures",
-                column: "FigureOptionId");
+        migrationBuilder.CreateIndex(
+            name: "IX_VerseChoreographiesFigures_FigureOptionId",
+            table: "VerseChoreographiesFigures",
+            column: "FigureOptionId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_VerseChoreographiesFigures_FigureOptions_FigureOptionId",
-                table: "VerseChoreographiesFigures",
-                column: "FigureOptionId",
-                principalTable: "FigureOptions",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_VerseChoreographiesFigures_FigureOptions_FigureOptionId",
+            table: "VerseChoreographiesFigures",
+            column: "FigureOptionId",
+            principalTable: "FigureOptions",
+            principalColumn: "Id",
+            onDelete: ReferentialAction.Cascade);
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_VerseChoreographiesFigures_FigureOptions_FigureOptionId",
-                table: "VerseChoreographiesFigures");
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_VerseChoreographiesFigures_FigureOptions_FigureOptionId",
+            table: "VerseChoreographiesFigures");
 
-            migrationBuilder.DropIndex(
-                name: "IX_VerseChoreographiesFigures_FigureOptionId",
-                table: "VerseChoreographiesFigures");
+        migrationBuilder.DropIndex(
+            name: "IX_VerseChoreographiesFigures_FigureOptionId",
+            table: "VerseChoreographiesFigures");
 
-            migrationBuilder.DropColumn(
-                name: "FigureOptionId",
-                table: "VerseChoreographiesFigures");
+        migrationBuilder.DropColumn(
+            name: "FigureOptionId",
+            table: "VerseChoreographiesFigures");
 
-            migrationBuilder.AddColumn<int>(
-                name: "FigureId",
-                table: "VerseChoreographiesFigures",
-                type: "int",
-                nullable: false,
-                defaultValue: 0,
-                comment: "Figure Identifier");
+        migrationBuilder.AddColumn<int>(
+            name: "FigureId",
+            table: "VerseChoreographiesFigures",
+            type: "int",
+            nullable: false,
+            defaultValue: 0,
+            comment: "Figure Identifier");
 
-            migrationBuilder.CreateIndex(
-                name: "IX_VerseChoreographiesFigures_FigureId",
-                table: "VerseChoreographiesFigures",
-                column: "FigureId");
+        migrationBuilder.CreateIndex(
+            name: "IX_VerseChoreographiesFigures_FigureId",
+            table: "VerseChoreographiesFigures",
+            column: "FigureId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_VerseChoreographiesFigures_Figures_FigureId",
-                table: "VerseChoreographiesFigures",
-                column: "FigureId",
-                principalTable: "Figures",
-                principalColumn: "Id",
-                onDelete: ReferentialAction.Cascade);
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_VerseChoreographiesFigures_Figures_FigureId",
+            table: "VerseChoreographiesFigures",
+            column: "FigureId",
+            principalTable: "Figures",
+            principalColumn: "Id",
+            onDelete: ReferentialAction.Cascade);
     }
 }
