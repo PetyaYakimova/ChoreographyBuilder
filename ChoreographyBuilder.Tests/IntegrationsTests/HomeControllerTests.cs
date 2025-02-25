929a5fa08434302c9268ab4e6b1ca7ee93b684c1
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-25 08:41:09 +0200

    Update namespace

--- ChoreographyBuilder.Tests/IntegrationsTests/HomeControllerTests.cs
+++ ChoreographyBuilder.Tests/IntegrationsTests/HomeControllerTests.cs
@@ -3,43 +3,42 @@ using ChoreographyBuilder.Tests.Mocks;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
-namespace ChoreographyBuilder.Tests.IntegrationsTests
+namespace ChoreographyBuilder.Tests.IntegrationsTests;
+
+[TestFixture]
+public class HomeControllerTests
 {
-	[TestFixture]
-	public class HomeControllerTests
+	private HomeController homeController;
+
+	[OneTimeSetUp]
+	public void OneTimeSetup()
 	{
-		private HomeController homeController;
+		this.homeController = new HomeController(UserServiceMock.Instance);
+	}
 
-		[OneTimeSetUp]
-		public void OneTimeSetup()
-		{
-			this.homeController = new HomeController(UserServiceMock.Instance);
-		}
+	[Test]
+	[TestCase(400)]
+	[TestCase(401)]
+	[TestCase(403)]
+	[TestCase(404)]
+	[TestCase(500)]
+	public void Error_ShouldReturnView(int statusCode)
+	{
+		var result = this.homeController.Error(statusCode);
 
-		[Test]
-		[TestCase(400)]
-		[TestCase(401)]
-		[TestCase(403)]
-		[TestCase(404)]
-		[TestCase(500)]
-		public void Error_ShouldReturnView(int statusCode)
-		{
-			var result = this.homeController.Error(statusCode);
+		Assert.IsNotNull(result);
 
-			Assert.IsNotNull(result);
+		var viewResult = result as ViewResult;
+		Assert.IsNotNull(viewResult);
+	}
 
-			var viewResult = result as ViewResult;
-			Assert.IsNotNull(viewResult);
-		}
+	[Test]
+	public void Index_ShouldReturnViewForNonRegisteredUsers()
+	{
+		var result = this.homeController.Index();
+		Assert.IsNotNull(result);
 
-		[Test]
-		public void Index_ShouldReturnViewForNonRegisteredUsers()
-		{
-			var result = this.homeController.Index();
-			Assert.IsNotNull(result);
-
-			var viewResult = result as ViewResult;
-			Assert.IsNotNull(viewResult);
-		}
+		var viewResult = result as ViewResult;
+		Assert.IsNotNull(viewResult);
 	}
 }
