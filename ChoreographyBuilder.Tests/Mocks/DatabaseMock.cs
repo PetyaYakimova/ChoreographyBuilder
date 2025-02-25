1ab59c2db54b45fb97de0533c3d1793cf689f7c1
Petya Yakimova <petya_yakimova@abv.bg>
2025-02-25 08:41:18 +0200

    Update namespace

--- ChoreographyBuilder.Tests/Mocks/DatabaseMock.cs
+++ ChoreographyBuilder.Tests/Mocks/DatabaseMock.cs
@@ -1,20 +1,19 @@
 ﻿using ChoreographyBuilder.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-namespace ChoreographyBuilder.Tests.Mocks
-{
-	public static class DatabaseMock
-	{
-		public static ChoreographyBuilderDbContext Instance
-		{
-			get
-			{
-				var dbContextOptions = new DbContextOptionsBuilder<ChoreographyBuilderDbContext>()
-					.UseInMemoryDatabase("ChoreographyBuilderInMemoryDb" + DateTime.Now.Ticks.ToString())
-					.Options;
+namespace ChoreographyBuilder.Tests.Mocks;
 
-				return new ChoreographyBuilderDbContext(dbContextOptions, false);
-			}
+public static class DatabaseMock
+{
+	public static ChoreographyBuilderDbContext Instance
+	{
+		get
+		{
+			var dbContextOptions = new DbContextOptionsBuilder<ChoreographyBuilderDbContext>()
+				.UseInMemoryDatabase("ChoreographyBuilderInMemoryDb" + DateTime.Now.Ticks.ToString())
+				.Options;
+
+			return new ChoreographyBuilderDbContext(dbContextOptions, false);
 		}
 	}
 }
