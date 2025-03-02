530a91f58118621c3c7477c80ae3f94dfdbd9dd3
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-02 11:47:50 +0200

    Update namespace

--- ChoreographyBuilder.Tests/UnitTests/VerseChoreographyFigureServiceTests.cs
+++ ChoreographyBuilder.Tests/UnitTests/VerseChoreographyFigureServiceTests.cs
@@ -1,111 +1,108 @@
-﻿using System.Collections;
-using AutoMapper;
-using ChoreographyBuilder.Core.Contracts;
+﻿using ChoreographyBuilder.Core.Contracts;
 using ChoreographyBuilder.Core.Exceptions;
 using ChoreographyBuilder.Core.Services;
 using Microsoft.Extensions.Logging;
 using Moq;
 
-namespace ChoreographyBuilder.Tests.UnitTests
+namespace ChoreographyBuilder.Tests.UnitTests;
+
+[TestFixture]
+public class VerseChoreographyFigureServiceTests : UnitTestsBase
 {
-	[TestFixture]
-	public class VerseChoreographyFigureServiceTests : UnitTestsBase
+	private IVerseChoreographyFigureService verseChoreographyFigureService;
+	private ILogger<VerseChoreographyFigureService> logger;
+
+	[SetUp]
+	public void Setup()
 	{
-		private IVerseChoreographyFigureService verseChoreographyFigureService;
-		private ILogger<VerseChoreographyFigureService> logger;
+		var mockLogger = new Mock<ILogger<VerseChoreographyFigureService>>();
+		this.logger = mockLogger.Object;
 
-		[SetUp]
-		public void Setup()
+		this.verseChoreographyFigureService = new VerseChoreographyFigureService(this.logger, repository, mapper);
+	}
+
+	[Test]
+	public async Task GetVerseChoreographyFigureForReplace_ShouldReturnValidVerseChoreographyFigureWithCorrectDataWhenIdExists()
+	{
+		var result = await verseChoreographyFigureService.GetVerseChoreographyFigureForReplaceAsync(FirstVerseChoreographyFirstFigure.Id);
+
+		Assert.Multiple(() =>
 		{
-			var mockLogger = new Mock<ILogger<VerseChoreographyFigureService>>();
-			this.logger = mockLogger.Object;
+			Assert.That(result.Id, Is.EqualTo((FirstVerseChoreography.Id)));
+			Assert.That(result.BeatsCount, Is.EqualTo(FirstFigureFirstOption.BeatCounts));
+			Assert.That(result.EndPosition, Is.EqualTo(FirstFigureFirstOption.EndPosition.Name));
+			Assert.That(result.StartPosition, Is.EqualTo(FirstFigureFirstOption.StartPosition.Name));
+			Assert.That(result.FigureOptionId, Is.EqualTo(FirstFigureFirstOption.Id));
+			Assert.That(result.FigureName, Is.EqualTo(FirstFigureFirstOption.Figure.Name));
+			Assert.That(result.FigureOrder, Is.EqualTo(FirstVerseChoreographyFirstFigure.FigureOrder));
+			Assert.That(result.DynamicsType, Is.EqualTo(FirstFigureFirstOption.DynamicsType.ToString()));
+			Assert.That(result.IsFavourite, Is.EqualTo(FirstFigure.IsFavourite));
+			Assert.That(result.IsHighlight, Is.EqualTo(FirstFigure.IsHighlight));
+		});
+	}
 
-			this.verseChoreographyFigureService = new VerseChoreographyFigureService(this.logger, repository, mapper);
-		}
+	[Test]
+	public void GetVerseChoreographyFigureForReplace_ShouldThrowExceptionWhenIdDoesntExists()
+	{
+		Assert.That(async () => await verseChoreographyFigureService.GetVerseChoreographyFigureForReplaceAsync(10),
+			Throws.Exception.TypeOf<EntityNotFoundException>());
+	}
 
-		[Test]
-		public async Task GetVerseChoreographyFigureForReplace_ShouldReturnValidVerseChoreographyFigureWithCorrectDataWhenIdExists()
+	[Test]
+	public async Task GetPossibleReplacementsForVerseChoreographyFigure_ShouldReturnValidCollectionWhenIdExists()
+	{
+		var result = await verseChoreographyFigureService.GetPossibleReplacementsForVerseChoreographyFigureAsync(FirstVerseChoreographyFirstFigure.Id);
+
+		Assert.Multiple(() =>
 		{
-			var result = await verseChoreographyFigureService.GetVerseChoreographyFigureForReplaceAsync(FirstVerseChoreographyFirstFigure.Id);
+			Assert.IsNotNull(result);
+			Assert.That(result.Count(), Is.EqualTo(1));
+		});
+	}
 
-			Assert.Multiple(() =>
-			{
-				Assert.That(result.Id, Is.EqualTo((FirstVerseChoreography.Id)));
-				Assert.That(result.BeatsCount, Is.EqualTo(FirstFigureFirstOption.BeatCounts));
-				Assert.That(result.EndPosition, Is.EqualTo(FirstFigureFirstOption.EndPosition.Name));
-				Assert.That(result.StartPosition, Is.EqualTo(FirstFigureFirstOption.StartPosition.Name));
-				Assert.That(result.FigureOptionId, Is.EqualTo(FirstFigureFirstOption.Id));
-				Assert.That(result.FigureName, Is.EqualTo(FirstFigureFirstOption.Figure.Name));
-				Assert.That(result.FigureOrder, Is.EqualTo(FirstVerseChoreographyFirstFigure.FigureOrder));
-				Assert.That(result.DynamicsType, Is.EqualTo(FirstFigureFirstOption.DynamicsType.ToString()));
-				Assert.That(result.IsFavourite, Is.EqualTo(FirstFigure.IsFavourite));
-				Assert.That(result.IsHighlight, Is.EqualTo(FirstFigure.IsHighlight));
-			});
-		}
+	[Test]
+	public void GetPossibleReplacementsForVerseChoreographyFigure_ShouldThrowExceptionWhenIdDoesntExists()
+	{
+		Assert.That(async () => await verseChoreographyFigureService.GetPossibleReplacementsForVerseChoreographyFigureAsync(10),
+			Throws.Exception.TypeOf<EntityNotFoundException>());
+	}
 
-		[Test]
-		public void GetVerseChoreographyFigureForReplace_ShouldThrowExceptionWhenIdDoesntExists()
-		{
-			Assert.That(async () => await verseChoreographyFigureService.GetVerseChoreographyFigureForReplaceAsync(10),
-				Throws.Exception.TypeOf<EntityNotFoundException>());
-		}
+	[Test]
+	public async Task GetVerseChoreographyIdForVerseChoreographyFigureById_ShouldReturnValidIdWhenIdExists()
+	{
+		var result = await verseChoreographyFigureService.GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(FirstVerseChoreographyFirstFigure.Id);
 
-		[Test]
-		public async Task GetPossibleReplacementsForVerseChoreographyFigure_ShouldReturnValidCollectionWhenIdExists()
-		{
-			var result = await verseChoreographyFigureService.GetPossibleReplacementsForVerseChoreographyFigureAsync(FirstVerseChoreographyFirstFigure.Id);
+		Assert.That(result, Is.EqualTo(FirstVerseChoreography.Id));
+	}
 
-			Assert.Multiple(() =>
-			{
-				Assert.IsNotNull(result);
-				Assert.That(result.Count(), Is.EqualTo(1));
-			});
-		}
+	[Test]
+	public void GetVerseChoreographyIdForVerseChoreographyFigureById_ShouldThrowExceptionWhenIdDoesntExists()
+	{
+		Assert.That(async () => await verseChoreographyFigureService.GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(10),
+			Throws.Exception.TypeOf<EntityNotFoundException>());
+	}
 
-		[Test]
-		public void GetPossibleReplacementsForVerseChoreographyFigure_ShouldThrowExceptionWhenIdDoesntExists()
-		{
-			Assert.That(async () => await verseChoreographyFigureService.GetPossibleReplacementsForVerseChoreographyFigureAsync(10),
-				Throws.Exception.TypeOf<EntityNotFoundException>());
-		}
+	[Test]
+	public async Task VerseChoreographyFigureExistsForThisUserById_ShouldReturnTrueForValidIdForThisUser()
+	{
+		var result = await verseChoreographyFigureService.VerseChoreographyFigureExistForThisUserByIdAsync(FirstVerseChoreographyFirstFigure.Id, FirstUser.Id);
 
-		[Test]
-		public async Task GetVerseChoreographyIdForVerseChoreographyFigureById_ShouldReturnValidIdWhenIdExists()
-		{
-			var result = await verseChoreographyFigureService.GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(FirstVerseChoreographyFirstFigure.Id);
+		Assert.IsTrue(result);
+	}
 
-			Assert.That(result, Is.EqualTo(FirstVerseChoreography.Id));
-		}
+	[Test]
+	public async Task VerseChoreographyFigureExistsForThisUserById_ShouldReturnFalseForInvalidId()
+	{
+		var result = await verseChoreographyFigureService.VerseChoreographyFigureExistForThisUserByIdAsync(10, FirstUser.Id);
 
-		[Test]
-		public void GetVerseChoreographyIdForVerseChoreographyFigureById_ShouldThrowExceptionWhenIdDoesntExists()
-		{
-			Assert.That(async () => await verseChoreographyFigureService.GetVerseChoreographyIdForVerseChoreographyFigureByIdAsync(10),
-				Throws.Exception.TypeOf<EntityNotFoundException>());
-		}
+		Assert.IsFalse(result);
+	}
 
-		[Test]
-		public async Task VerseChoreographyFigureExistsForThisUserById_ShouldReturnTrueForValidIdForThisUser()
-		{
-			var result = await verseChoreographyFigureService.VerseChoreographyFigureExistForThisUserByIdAsync(FirstVerseChoreographyFirstFigure.Id, FirstUser.Id);
+	[Test]
+	public async Task VerseChoreographyFigureExistsForThisUserById_ShouldReturnFalseForValidIdForAnotherUserVerseChoreographyFigure()
+	{
+		var result = await verseChoreographyFigureService.VerseChoreographyFigureExistForThisUserByIdAsync(FirstVerseChoreographyFirstFigure.Id, SecondUser.Id);
 
-			Assert.IsTrue(result);
-		}
-
-		[Test]
-		public async Task VerseChoreographyFigureExistsForThisUserById_ShouldReturnFalseForInvalidId()
-		{
-			var result = await verseChoreographyFigureService.VerseChoreographyFigureExistForThisUserByIdAsync(10, FirstUser.Id);
-
-			Assert.IsFalse(result);
-		}
-
-		[Test]
-		public async Task VerseChoreographyFigureExistsForThisUserById_ShouldReturnFalseForValidIdForAnotherUserVerseChoreographyFigure()
-		{
-			var result = await verseChoreographyFigureService.VerseChoreographyFigureExistForThisUserByIdAsync(FirstVerseChoreographyFirstFigure.Id, SecondUser.Id);
-
-			Assert.IsFalse(result);
-		}
+		Assert.IsFalse(result);
 	}
 }
