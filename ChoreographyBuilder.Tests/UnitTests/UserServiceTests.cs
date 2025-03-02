8a609a1e734434d8cdb6d7c71864954044b6a977
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-02 11:47:38 +0200

    Update namespace

--- ChoreographyBuilder.Tests/UnitTests/UserServiceTests.cs
+++ ChoreographyBuilder.Tests/UnitTests/UserServiceTests.cs
@@ -1,74 +1,73 @@
 ﻿using ChoreographyBuilder.Core.Contracts;
 using ChoreographyBuilder.Core.Services;
 
-namespace ChoreographyBuilder.Tests.UnitTests
+namespace ChoreographyBuilder.Tests.UnitTests;
+
+[TestFixture]
+public class UserServiceTests : UnitTestsBase
 {
-	[TestFixture]
-	public class UserServiceTests : UnitTestsBase
+	private IUserService userService;
+
+	[SetUp]
+	public void Setup()
 	{
-		private IUserService userService;
+		this.userService = new UserService(repository);
+	}
 
-		[SetUp]
-		public void Setup()
+	[Test]
+	public async Task GetAdminStatistics_ShouldReturnTheCorrectData()
+	{
+		var result = await userService.GetAdminStatisticsAsync();
+
+		Assert.Multiple(() =>
 		{
-			this.userService = new UserService(repository);
-		}
+			Assert.That(result.NumberOfActivePositions, Is.EqualTo(data.Positions.Count(p => p.IsActive)));
+			Assert.That(result.TotalNumberOfPositions, Is.EqualTo(data.Positions.Count()));
+			Assert.That(result.NumberOfActiveVerseTypes, Is.EqualTo(data.VerseTypes.Count(v => v.IsActive)));
+			Assert.That(result.TotalNumberOfVerseTypes, Is.EqualTo(data.VerseTypes.Count()));
+			Assert.That(result.TotalNumberOfFigures, Is.EqualTo(data.Figures.Count()));
+			Assert.That(result.UsersWithAtLeastOneFigure, Is.EqualTo(2));
+			Assert.That(result.TotalNumberOfSavedVerseChoreographies, Is.EqualTo(data.VerseChoreographies.Count()));
+			Assert.That(result.UsersWithAtLeastOneVerseChoreography, Is.EqualTo(2));
+			Assert.That(result.TotalNumberOfSavedFullChoreographies, Is.EqualTo(data.FullChoreographies.Count()));
+			Assert.That(result.UsersWithAtLeastOneFullChoreography, Is.EqualTo(1));
+		});
+	}
 
-		[Test]
-		public async Task GetAdminStatistics_ShouldReturnTheCorrectData()
+	[Test]
+	public async Task GetUserStatistics_ShouldReturnTheCorrectData()
+	{
+		var result = await userService.GetUserStatisticsAsync(FirstUser.Id);
+
+		Assert.Multiple(() =>
 		{
-			var result = await userService.GetAdminStatisticsAsync();
+			Assert.That(result.MyTotalNumberOfFigures, Is.EqualTo(data.Figures.Count(f => f.UserId == FirstUser.Id)));
+			Assert.That(result.MyTotalNumberOfVerseChoreographies, Is.EqualTo(data.VerseChoreographies.Count(v => v.UserId == FirstUser.Id)));
+			Assert.That(result.MyTotalNumberOfFullChoreographies, Is.EqualTo(data.FullChoreographies.Count(f => f.UserId == FirstUser.Id)));
+		});
+	}
 
-			Assert.Multiple(() =>
-			{
-				Assert.That(result.NumberOfActivePositions, Is.EqualTo(data.Positions.Count(p => p.IsActive)));
-				Assert.That(result.TotalNumberOfPositions, Is.EqualTo(data.Positions.Count()));
-				Assert.That(result.NumberOfActiveVerseTypes, Is.EqualTo(data.VerseTypes.Count(v => v.IsActive)));
-				Assert.That(result.TotalNumberOfVerseTypes, Is.EqualTo(data.VerseTypes.Count()));
-				Assert.That(result.TotalNumberOfFigures, Is.EqualTo(data.Figures.Count()));
-				Assert.That(result.UsersWithAtLeastOneFigure, Is.EqualTo(2));
-				Assert.That(result.TotalNumberOfSavedVerseChoreographies, Is.EqualTo(data.VerseChoreographies.Count()));
-				Assert.That(result.UsersWithAtLeastOneVerseChoreography, Is.EqualTo(2));
-				Assert.That(result.TotalNumberOfSavedFullChoreographies, Is.EqualTo(data.FullChoreographies.Count()));
-				Assert.That(result.UsersWithAtLeastOneFullChoreography, Is.EqualTo(1));
-			});
-		}
+	[Test]
+	public async Task GetAllUserStatistics_ShouldReturnTheCorrectDataForAllUsersWhenNoSearchCriteria()
+	{
+		var result = await userService.GetAllUserStatisticsAsync();
 
-		[Test]
-		public async Task GetUserStatistics_ShouldReturnTheCorrectData()
+		Assert.Multiple(() =>
 		{
-			var result = await userService.GetUserStatisticsAsync(FirstUser.Id);
+			Assert.That(result.TotalCount, Is.EqualTo(data.Users.Count()));
+			Assert.That(result.Entities.Count(), Is.EqualTo(data.Users.Count()));
+		});
+	}
 
-			Assert.Multiple(() =>
-			{
-				Assert.That(result.MyTotalNumberOfFigures, Is.EqualTo(data.Figures.Count(f => f.UserId == FirstUser.Id)));
-				Assert.That(result.MyTotalNumberOfVerseChoreographies, Is.EqualTo(data.VerseChoreographies.Count(v => v.UserId == FirstUser.Id)));
-				Assert.That(result.MyTotalNumberOfFullChoreographies, Is.EqualTo(data.FullChoreographies.Count(f => f.UserId == FirstUser.Id)));
-			});
-		}
+	[Test]
+	public async Task GetAllUserStatistics_ShouldReturnTheCorrectDataForSomeUsersWhenSearchCriteriaIsAdded()
+	{
+		var result = await userService.GetAllUserStatisticsAsync("First");
 
-		[Test]
-		public async Task GetAllUserStatistics_ShouldReturnTheCorrectDataForAllUsersWhenNoSearchCriteria()
+		Assert.Multiple(() =>
 		{
-			var result = await userService.GetAllUserStatisticsAsync();
-
-			Assert.Multiple(() =>
-			{
-				Assert.That(result.TotalCount, Is.EqualTo(data.Users.Count()));
-				Assert.That(result.Entities.Count(), Is.EqualTo(data.Users.Count()));
-			});
-		}
-
-		[Test]
-		public async Task GetAllUserStatistics_ShouldReturnTheCorrectDataForSomeUsersWhenSearchCriteriaIsAdded()
-		{
-			var result = await userService.GetAllUserStatisticsAsync("First");
-
-			Assert.Multiple(() =>
-			{
-				Assert.That(result.TotalCount, Is.EqualTo(1));
-				Assert.That(result.Entities.Count(), Is.EqualTo(1));
-			});
-		}
+			Assert.That(result.TotalCount, Is.EqualTo(1));
+			Assert.That(result.Entities.Count(), Is.EqualTo(1));
+		});
 	}
 }
