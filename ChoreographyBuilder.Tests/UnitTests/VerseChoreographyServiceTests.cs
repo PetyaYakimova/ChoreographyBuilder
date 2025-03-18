507ca9ef499845468f47f9e5a6f6ff9ddb35a881
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-18 08:36:35 +0200

    Add test for get start position name when choreo has no figures

--- ChoreographyBuilder.Tests/UnitTests/VerseChoreographyServiceTests.cs
+++ ChoreographyBuilder.Tests/UnitTests/VerseChoreographyServiceTests.cs
@@ -130,6 +130,14 @@ public class VerseChoreographyServiceTests : UnitTestsBase
     }
 
     [Test]
+    public async Task GetStartPositionNameForVerseChoreography_ShouldReturnNullWhenChoreoExistsButHasNoFigures()
+    {
+        var result = await verseChoreographyService.GetStartPositionNameForVerseChoreographyAsync(FourthVerseChoreography.Id);
+
+        Assert.That(result, Is.EqualTo(null));
+    }
+
+    [Test]
     public async Task AllUserVerseChoreographies_ShouldReturnAllUserVerseChoreographiesWhenThereAreNoSearchCriteria()
     {
         var expectedCount = this.data.VerseChoreographies.Count(c => c.UserId == FirstUser.Id);
