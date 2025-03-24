35191f75fb20b17a8be9c4471d1d70a70ed173ff
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-24 09:05:26 +0200

    Add app settings class

---
+++ ChoreographyBuilder.UITests/Setup/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace ChoreographyBuilder.UITests.Setup;
+
+public class AppSettings
+{
+	public DomainSettings DomainSettings { get; set; } = null!;
+
+	public ConnectionStrings ConnectionStrings { get; set; } = null!;
+}
+
+public class DomainSettings
+{
+	public string Domain { get; set; } = null!;
+}
+
+public class ConnectionStrings
+{
+	public string DBConnectionString { get; set; } = null!;
+}
