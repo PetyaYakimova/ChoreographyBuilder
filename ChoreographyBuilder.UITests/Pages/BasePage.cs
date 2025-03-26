77a994bb2088cf7ae30e343c1ff60759a11074f1
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-26 09:14:18 +0200

    Add base page with basic methods

---
+++ ChoreographyBuilder.UITests/Pages/BasePage.cs
@@ -0,0 +1,39 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class BasePage
+{
+    protected readonly AppSettings settings;
+    protected readonly IWebDriver driver;
+    protected readonly WebDriverWait wait;
+
+    protected BasePage(
+        AppSettings settings,
+        IWebDriver driver,
+        WebDriverWait wait)
+    {
+        this.settings = settings;
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public void OpenHomePage()
+    {
+        driver.Navigate().GoToUrl(settings.DomainSettings.Domain);
+    }
+
+    public void OpenPage(string pageName)
+    {
+        pageName = pageName.Replace(" ", "_");
+        driver.Navigate().GoToUrl(settings.DomainSettings.Domain + pageName);
+    }
+
+    public string GetCurrentURL()
+    {
+        return driver.Url;
+    }
+
+}
