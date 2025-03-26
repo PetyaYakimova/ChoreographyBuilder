640f9558bd7850068419eca6288177aa02e64228
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-26 09:18:00 +0200

    Add base step definitions class

---
+++ ChoreographyBuilder.UITests/StepDefinitions/BaseStepDefinitions.cs
@@ -0,0 +1,17 @@
+﻿using TechTalk.SpecFlow;
+
+namespace ChoreographyBuilder.UITests.StepDefinitions;
+
+[Binding]
+public class BaseStepDefinitions
+{
+    public BaseStepDefinitions()
+    {
+
+    }
+
+    protected bool GetBooleanFromString(string value)
+    {
+        return value.ToLower().Contains("not") ? false : true;
+    }
+}
