964c30f10a3b3e5d69f4ac24419c456badfe28cf
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-27 08:55:53 +0200

    Add seed data repository

---
+++ ChoreographyBuilder.UITests/Repositories/SeedDataRepository.cs
@@ -0,0 +1,20 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class SeedDataRepository : BaseRepository
+{
+    public SeedDataRepository(AppSettings settings) : base(settings)
+    {
+    }
+
+    public void SeedInitialUsersData()
+    {
+        
+    }
+
+    public void DeleteSeededData()
+    {
+        
+    }
+}
