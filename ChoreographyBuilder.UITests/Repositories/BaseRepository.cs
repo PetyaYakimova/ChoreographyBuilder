754598f39061e85c64d9989d1bfbedaaf2794916
Petya Yakimova <petya_yakimova@abv.bg>
2025-03-27 08:53:06 +0200

    Add base repository

---
+++ ChoreographyBuilder.UITests/Repositories/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using ChoreographyBuilder.Infrastructure.Data;
+using ChoreographyBuilder.UITests.Setup;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class BaseRepository
+{
+    public ChoreographyBuilderDbContext context;
+    public DbContextOptionsBuilder<ChoreographyBuilderDbContext> optionsBuilder;
+
+    public BaseRepository(AppSettings settings)
+    {
+        optionsBuilder = new DbContextOptionsBuilder<ChoreographyBuilderDbContext>();
+        optionsBuilder.UseSqlServer(settings.ConnectionStrings.DBConnectionString);
+        context = new ChoreographyBuilderDbContext(optionsBuilder.Options);
+    }
+
+    public void CreateContext()
+    {
+        context = new ChoreographyBuilderDbContext(optionsBuilder.Options);
+    }
+}
