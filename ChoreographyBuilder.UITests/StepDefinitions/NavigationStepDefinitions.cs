530ac3b284ba68225868ba86cbc787cbfbd577c3
Petya Yakimova <petya_yakimova@abv.bg>
2025-04-13 12:08:29 +0300

    Add navigation step definitions

---
+++ ChoreographyBuilder.UITests/StepDefinitions/NavigationStepDefinitions.cs
@@ -0,0 +1,20 @@
+﻿using ChoreographyBuilder.UITests.Pages;
+using TechTalk.SpecFlow;
+
+namespace ChoreographyBuilder.UITests.StepDefinitions;
+
+public class NavigationStepDefinitions : BaseStepDefinitions
+{
+    private readonly BasePage basePage;
+
+    public NavigationStepDefinitions(BasePage basePage) : base()
+    {
+        this.basePage = basePage;
+    }
+
+    [StepDefinition($"I open the (.*) page")]
+    public void OpenPage(string pageName)
+    {
+       basePage.OpenPage(pageName);
+    }
+}
