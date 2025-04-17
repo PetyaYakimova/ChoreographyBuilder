514307d23dc2bdde9f25df391bb5053fa0668843
Petya Yakimova <petya_yakimova@abv.bg>
2025-04-17 13:44:24 +0300

    Add RegisterPage

---
+++ ChoreographyBuilder.UITests/Pages/RegisterPage.cs
@@ -0,0 +1,14 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class RegisterPage : BasePage
+{
+    public RegisterPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
+    {
+    }
+
+
+}
