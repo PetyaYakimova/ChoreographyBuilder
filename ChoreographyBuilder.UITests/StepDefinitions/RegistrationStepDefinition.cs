63664a39e49c7af88b9bb026619a796b45d6f095
Petya Yakimova <petya_yakimova@abv.bg>
2025-04-21 10:00:52 +0300

    Add registration step deifinitions

---
+++ ChoreographyBuilder.UITests/StepDefinitions/RegistrationStepDefinition.cs
@@ -0,0 +1,26 @@
+﻿using ChoreographyBuilder.UITests.Pages;
+using TechTalk.SpecFlow;
+
+namespace ChoreographyBuilder.UITests.StepDefinitions;
+
+public class RegistrationStepDefinition : BaseStepDefinitions
+{
+    private readonly RegisterPage registerPage;
+
+    public RegistrationStepDefinition(RegisterPage registerPage) : base()
+    {
+        this.registerPage = registerPage;
+    }
+
+    [StepDefinition($"I fill the registration form with email (.*), password (.*), confirm password (.*)")]
+    public void IFillRegistrationFormWithData(string email, string password, string confirmPassword)
+    {
+        registerPage.FillRegisterForm(email, password, confirmPassword);
+    }
+
+    [StepDefinition(@"I click the Register button")]
+    public void ClickTheRegisterButton()
+    {
+        registerPage.ClickRegisterButton();
+    }
+}
