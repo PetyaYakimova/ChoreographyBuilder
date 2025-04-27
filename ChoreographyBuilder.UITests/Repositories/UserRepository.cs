2119a0d3af91d3514646ad1371a45b0b30361a5c
Petya Yakimova <petya_yakimova@abv.bg>
2025-04-27 10:24:47 +0300

    Add user repository

---
+++ ChoreographyBuilder.UITests/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class UserRepository : BaseRepository
+{
+    public UserRepository(AppSettings settings) : base(settings)
+    {
+    }
+
+    public bool IsUserSaved(string email)
+        => context.Users.Any(u => u.Email == email);
+}
