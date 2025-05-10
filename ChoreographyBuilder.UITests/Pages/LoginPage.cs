27a7ec19ce0098285f5548e0b1d36395a21f9b8b
Petya Yakimova <petya_yakimova@abv.bg>
2025-05-10 09:58:22 +0300

    Add locators and methods in login page

--- ChoreographyBuilder.UITests/Pages/LoginPage.cs
+++ ChoreographyBuilder.UITests/Pages/LoginPage.cs
@@ -9,4 +9,34 @@ public class LoginPage : BasePage
     public LoginPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
     {
     }
+
+    private IWebElement EmailField => driver.FindElement(EmailFieldBy);
+    private By EmailFieldBy => By.Id("email");
+
+    private IWebElement PasswordField => driver.FindElement(PasswordFieldBy);
+    private By PasswordFieldBy => By.Id("password");
+
+    private IWebElement LoginButton => driver.FindElement(LoginButtonBy);
+    private By LoginButtonBy => By.Id("login-submit");
+
+    public void LoginAsUser(string user)
+    {
+        OpenPage("Identity/Account/Login");
+
+    }
+
+    public void FillEmailField(string email)
+    {
+        EmailField.Clear();
+        EmailField.SendKeys(email);
+    }
+
+    public void FillPasswordField(string password)
+    {
+        PasswordField.Clear();
+        PasswordField.SendKeys(password);
+    }
+
+    public void ClickLoginButton()
+        => LoginButton.Click();
 }
