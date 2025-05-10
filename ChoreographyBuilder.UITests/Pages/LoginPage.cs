5bff512a4b5a26c68c950137dbfb0248be426828
Petya Yakimova <petya_yakimova@abv.bg>
2025-05-10 09:54:44 +0300

    Add login page

---
+++ ChoreographyBuilder.UITests/Pages/LoginPage.cs
@@ -0,0 +1,12 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class LoginPage : BasePage
+{
+    public LoginPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
+    {
+    }
+}
