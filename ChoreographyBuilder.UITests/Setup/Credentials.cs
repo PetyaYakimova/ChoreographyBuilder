230ebcb9f44609d54a0eeaf773cdd943b2a28e77
Petya Yakimova <petya_yakimova@abv.bg>
2025-05-11 10:03:01 +0300

    Add credentials

---
+++ ChoreographyBuilder.UITests/Setup/Credentials.cs
@@ -0,0 +1,15 @@
+﻿using ChoreographyBuilder.UITests.Models;
+
+namespace ChoreographyBuilder.UITests.Setup;
+
+public static class Credentials
+{
+    //TODO: Check how its done in F2F
+    public static UserModel FirstUser()
+    {
+        return new UserModel();
+        {
+
+        }
+    }
+}
