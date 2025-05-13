4d3e1e79dd58739f4b1bc21e8931427c147abdae
Petya Yakimova <petya_yakimova@abv.bg>
2025-05-13 08:31:26 +0300

    Add login steps

---
+++ ChoreographyBuilder.UITests/StepDefinitions/LoginStepDefinitions.cs
@@ -0,0 +1,20 @@
+﻿using ChoreographyBuilder.UITests.Pages;
+using TechTalk.SpecFlow;
+
+namespace ChoreographyBuilder.UITests.StepDefinitions;
+
+public class LoginStepDefinitions : BaseStepDefinitions
+{
+    private readonly LoginPage loginPage;
+
+    public LoginStepDefinitions(LoginPage loginPage) : base()
+    {
+        this.loginPage = loginPage;
+    }
+
+    [StepDefinition($"I log in as (.*)")]
+    public void ILogInAs(string user)
+    {
+        loginPage.LogInAsUser(user);
+    }
+}
