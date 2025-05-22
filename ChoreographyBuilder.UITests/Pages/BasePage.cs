24607bc065604dcd58edd0aaf1b6c02651d3b53b
Petya Yakimova <petya_yakimova@abv.bg>
2025-05-22 09:18:33 +0300

    Add methods in base page

--- ChoreographyBuilder.UITests/Pages/BasePage.cs
+++ ChoreographyBuilder.UITests/Pages/BasePage.cs
@@ -6,53 +6,56 @@ namespace ChoreographyBuilder.UITests.Pages;
 
 public class BasePage
 {
-	protected readonly AppSettings settings;
-	protected readonly IWebDriver driver;
-	protected readonly WebDriverWait wait;
+    protected readonly AppSettings settings;
+    protected readonly IWebDriver driver;
+    protected readonly WebDriverWait wait;
 
-	protected BasePage(
-		AppSettings settings,
-		IWebDriver driver,
-		WebDriverWait wait)
-	{
-		this.settings = settings;
-		this.driver = driver;
-		this.wait = wait;
-	}
+    protected BasePage(
+        AppSettings settings,
+        IWebDriver driver,
+        WebDriverWait wait)
+    {
+        this.settings = settings;
+        this.driver = driver;
+        this.wait = wait;
+    }
 
-	private IWebElement HeaderGreetingText => driver.FindElement(HeaderGreetingTextBy);
-	private By HeaderGreetingTextBy => By.Id("greeting");
+    private IWebElement HeaderGreetingText => driver.FindElement(HeaderGreetingTextBy);
+    private By HeaderGreetingTextBy => By.Id("greeting");
 
-	private IWebElement HederMenuItem => driver.FindElement(HederMenuItemBy);
-	private By HederMenuItemBy => By.ClassName("nav-menu-item");
+    private IWebElement HederMenuItem => driver.FindElement(HederMenuItemBy);
+    private By HederMenuItemBy => By.ClassName("nav-menu-item");
 
-	public void OpenHomePage()
-		=> driver.Navigate().GoToUrl(settings.DomainSettings.Domain);
+    public void OpenHomePage()
+        => driver.Navigate().GoToUrl(settings.DomainSettings.Domain);
 
-	public void OpenPage(string pageName)
-		=> driver.Navigate().GoToUrl(settings.DomainSettings.Domain + pageName);
+    public void OpenPage(string pageName)
+        => driver.Navigate().GoToUrl(settings.DomainSettings.Domain + pageName);
 
-	public string GetCurrentURL()
-		=> driver.Url;
+    public string GetCurrentURL()
+        => driver.Url;
 
-	public string GetCurrentPage()
-		=> GetCurrentURL().Replace(settings.DomainSettings.Domain, string.Empty);
+    public string GetCurrentPage()
+        => GetCurrentURL().Replace(settings.DomainSettings.Domain, string.Empty);
 
-	public string GetGreetingFromHeader()
-		=> HeaderGreetingText.Text;
+    public string GetGreetingFromHeader()
+        => HeaderGreetingText.Text;
 
-	public List<string> GetHeaderMenus()
-		=> driver.FindElements(HederMenuItemBy).Select(e => e.Text).ToList();
+    public List<string> GetHeaderMenus()
+        => driver.FindElements(HederMenuItemBy).Select(e => e.Text).ToList();
 
-	public bool DoesElementExistAndIsDisplayed(By locator)
-	{
-		try
-		{
-			return driver.FindElement(locator).Displayed;
-		}
-		catch (NoSuchElementException)
-		{
-			return false;
-		}
-	}
+    public void ClickOnMenu(string menuName)
+        => driver.FindElement(By.XPath($"//header//a[contains(text(), '{menuName}')]")).Click();
+
+    public bool DoesElementExistAndIsDisplayed(By locator)
+    {
+        try
+        {
+            return driver.FindElement(locator).Displayed;
+        }
+        catch (NoSuchElementException)
+        {
+            return false;
+        }
+    }
 }
