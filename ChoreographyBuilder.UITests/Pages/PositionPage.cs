4ff1f588262d9a3a90342fcce1a9125948727866
Petya Yakimova <petya_yakimova@abv.bg>
2025-06-07 09:33:59 +0300

    Add position page

---
+++ ChoreographyBuilder.UITests/Pages/PositionPage.cs
@@ -0,0 +1,21 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class PositionPage : BasePage
+{
+    public PositionPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
+    {
+    }
+
+    private IWebElement AddPage_NameField => driver.FindElement(AddPage_NameFieldBy);
+    private By AddPage_NameFieldBy => By.Id("Name");
+
+    public void FillNameField(string name)
+    {
+        AddPage_NameField.Clear();
+        AddPage_NameField.SendKeys(name);
+    }
+}
