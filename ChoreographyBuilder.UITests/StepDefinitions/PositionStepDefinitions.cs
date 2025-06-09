02d8f2461a225514ef476d3ab25ffb6ba20f24f2
Petya Yakimova <petya_yakimova@abv.bg>
2025-06-09 08:32:24 +0300

    Add position step definition class

---
+++ ChoreographyBuilder.UITests/StepDefinitions/PositionStepDefinitions.cs
@@ -0,0 +1,20 @@
+﻿using ChoreographyBuilder.UITests.Pages;
+using TechTalk.SpecFlow;
+
+namespace ChoreographyBuilder.UITests.StepDefinitions;
+
+public class PositionStepDefinitions : BaseStepDefinitions
+{
+    private readonly PositionPage positionPage;
+
+    public PositionStepDefinitions(PositionPage positionPage) : base()
+    {
+        this.positionPage = positionPage;
+    }
+
+    [StepDefinition(@"I fill the name field for position with (.*)")]
+    public void IFillTheNameFieldForPositionWith(string name)
+    {
+        positionPage.FillNameField(name);
+    }
+}
