6e5554c5d3354791d07165088d495bfbb76e50bb
Petya Yakimova <petya_yakimova@abv.bg>
2025-06-10 09:20:49 +0300

    Add position repository

---
+++ ChoreographyBuilder.UITests/Repositories/PositionRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChoreographyBuilder.Infrastructure.Data.Models;
+using ChoreographyBuilder.UITests.Setup;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class PositionRepository : BaseRepository
+{
+    public PositionRepository(AppSettings settings) : base(settings)
+    {
+    }
+
+    public Position? GetPositionByName(string name)
+    {
+        UpdateContext();
+        return context.Positions.FirstOrDefault(p => p.Name == name);
+    }
+}
