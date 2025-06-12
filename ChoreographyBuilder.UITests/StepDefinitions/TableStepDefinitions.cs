b9d42e76cdbe1510354eb758be0ada06923765e9
Petya Yakimova <petya_yakimova@abv.bg>
2025-06-12 09:07:11 +0300

    Add table step definitions

---
+++ ChoreographyBuilder.UITests/StepDefinitions/TableStepDefinitions.cs
@@ -0,0 +1,20 @@
+﻿using ChoreographyBuilder.UITests.Pages;
+using TechTalk.SpecFlow;
+
+namespace ChoreographyBuilder.UITests.StepDefinitions;
+
+public class TableStepDefinitions : BaseStepDefinitions
+{
+    private readonly BasePage basePage;
+
+    public TableStepDefinitions(BasePage basePage) : base()
+    {
+        this.basePage = basePage;
+    }
+
+    [StepDefinition($"I search in the table by (.*)")]
+    public void SearchInTableBySearchTerm(string searchTerm)
+    {
+        basePage.SearchInTableBySearchTerm(searchTerm);
+    }
+}
