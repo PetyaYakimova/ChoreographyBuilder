ee13dd3895381c139ba164c592a08fa4e072ce42
Petya Yakimova <petya_yakimova@abv.bg>
2025-06-30 07:45:43 +0300

    Remove duplicate code that can be used in the base page

--- ChoreographyBuilder.UITests/Pages/RegisterPage.cs
+++ ChoreographyBuilder.UITests/Pages/RegisterPage.cs
@@ -1,6 +1,7 @@
 ﻿using ChoreographyBuilder.UITests.Repositories;
 using ChoreographyBuilder.UITests.Setup;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
 using OpenQA.Selenium.Support.UI;
 
 namespace ChoreographyBuilder.UITests.Pages;
@@ -17,20 +18,11 @@ public class RegisterPage : BasePage
     private IWebElement EmailField => driver.FindElement(EmailFieldBy);
     private By EmailFieldBy => By.Id("email");
 
-    private IWebElement EmailFieldError => driver.FindElement(EmailFieldErrorBy);
-    private By EmailFieldErrorBy => By.Id("email-error");
-
     private IWebElement PasswordField => driver.FindElement(PasswordFieldBy);
     private By PasswordFieldBy => By.Id("password");
 
-    private IWebElement PasswordFieldError => driver.FindElement(PasswordFieldErrorBy);
-    private By PasswordFieldErrorBy => By.Id("password-error");
-
     private IWebElement ConfirmPasswordField => driver.FindElement(ConfirmPasswordFieldBy);
     private By ConfirmPasswordFieldBy => By.Id("confirmPassword");
-
-    private IWebElement ConfirmPasswordFieldError => driver.FindElement(ConfirmPasswordFieldErrorBy);
-    private By ConfirmPasswordFieldErrorBy => By.Id("confirmPassword-error");
 
     private IWebElement RegisterButton => driver.FindElement(RegisterButtonBy);
     private By RegisterButtonBy => By.Id("registerSubmit");
@@ -41,17 +33,11 @@ public class RegisterPage : BasePage
         EmailField.SendKeys(email);
     }
 
-    public string GetEmailFieldErrorText()
-        => EmailFieldError.Text;
-
     public void FillPasswordField(string password)
     {
         PasswordField.Clear();
         PasswordField.SendKeys(password);
     }
-
-    public string GetPasswordFieldErrorText()
-       => PasswordFieldError.Text;
 
     public void FillConfirmPasswordField(string confirmPassword)
     {
@@ -59,11 +45,11 @@ public class RegisterPage : BasePage
         ConfirmPasswordField.SendKeys(confirmPassword);
     }
 
-    public string GetConfirmPasswordFieldErrorText()
-        => ConfirmPasswordFieldError.Text;
-
     public void ClickRegisterButton()
-        => RegisterButton.Click();
+    {
+        driver.ExecuteJavaScript("arguments[0].scrollIntoView(true);", RegisterButton);
+        RegisterButton.Click();
+    }
 
     public void FillRegisterForm(string email, string password, string confirmPassword)
     {
