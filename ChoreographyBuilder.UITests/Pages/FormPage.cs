541a79ad90df149220b4164bc48d91a0cd3ff45b
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-04 08:22:46 +0300

    Create form page

---
+++ ChoreographyBuilder.UITests/Pages/FormPage.cs
@@ -0,0 +1,27 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class FormPage : BasePage
+{
+    public FormPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
+    {
+    }
+
+    public void FillField(string fieldName, string value)
+    {
+        IWebElement field = driver.FindElement(By.Id(fieldName));
+        field.Clear();
+        field.SendKeys(value);
+    }
+    public void ClearField(string fieldName)
+        => driver.FindElement(By.Id(fieldName)).Clear();
+
+    public string GetValidationErrorMessage(string fieldName)
+    {
+        var errorElement = driver.FindElement(By.Id($"{fieldName}-error"));
+        return errorElement.Text;
+    }
+}
