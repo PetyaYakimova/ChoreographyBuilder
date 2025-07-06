5b6397ac60b6b2856222a4543bd05e887e0bfc1b
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-06 10:21:35 +0300

    Add verse type page

---
+++ ChoreographyBuilder.UITests/Pages/VerseTypePage.cs
@@ -0,0 +1,34 @@
+﻿using ChoreographyBuilder.Infrastructure.Data.Models;
+using ChoreographyBuilder.UITests.Models;
+using ChoreographyBuilder.UITests.Repositories;
+using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class VerseTypePage : BasePage
+{
+    private VerseTypeRepository verseTypeRepository;
+
+    public VerseTypePage(AppSettings settings, IWebDriver driver, WebDriverWait wait, VerseTypeRepository verseTypeRepository) : base(settings, driver, wait)
+    {
+        this.verseTypeRepository = verseTypeRepository;
+    }
+
+    public VerseType? GetVerseTypeFromDbByName(string name)
+        => verseTypeRepository.GetVerseTypeByName(name);
+
+    public List<PositionFromTableModel> GetPositionsFromTable()
+    {
+        IEnumerable<List<string>> tableData = GetTableRowsData();
+        List<PositionFromTableModel> positions = tableData.Select(row =>
+            new PositionFromTableModel()
+            {
+                Name = row[0],
+            })
+            .ToList();
+
+        return positions;
+    }
+}
