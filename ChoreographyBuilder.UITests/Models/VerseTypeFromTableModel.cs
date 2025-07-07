399e2a8b91d24a03eecfbd081fcf969fb209afe9
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-07 08:24:56 +0300

    Add verse type from table model

---
+++ ChoreographyBuilder.UITests/Models/VerseTypeFromTableModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ChoreographyBuilder.UITests.Models;
+
+public class VerseTypeFromTableModel
+{
+    public string Name { get; set; } = null!;
+
+    public string BeatCounts { get; set; } = null!;
+}
