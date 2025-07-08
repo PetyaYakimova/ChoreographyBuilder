b1e943ce2dbf02cd8f9beffaeb36bb948b6daf54
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-08 08:23:37 +0300

    Add verse type repository

---
+++ ChoreographyBuilder.UITests/Repositories/VerseTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChoreographyBuilder.Infrastructure.Data.Models;
+using ChoreographyBuilder.UITests.Setup;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class VerseTypeRepository : BaseRepository
+{
+    public VerseTypeRepository(AppSettings settings) : base(settings)
+    {
+    }
+
+    public VerseType? GetVerseTypeByName(string name)
+    {
+        UpdateContext();
+        return context.VerseTypes.FirstOrDefault(vt => vt.Name == name);
+    }
+}
