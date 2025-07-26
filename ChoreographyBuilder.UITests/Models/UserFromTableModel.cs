80a31ad7e7490702594752989cca39abe2b445d0
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-26 10:51:42 +0300

    Add users from table model

---
+++ ChoreographyBuilder.UITests/Models/UserFromTableModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ChoreographyBuilder.UITests.Models;
+
+public class UserFromTableModel
+{
+    public string Email { get; set; } = null!;
+
+    public int Figures { get; set; }
+
+    public int VerseChoreographies { get; set; }
+
+    public int FullChoreographies { get; set; }
+}
