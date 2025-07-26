5a8d0a538cc6263b8a089e5dcbf98989290ae146
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-26 10:51:33 +0300

    Add user page

---
+++ ChoreographyBuilder.UITests/Pages/UserPage.cs
@@ -0,0 +1,29 @@
+﻿using ChoreographyBuilder.UITests.Models;
+using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class UserPage : BasePage
+{
+    public UserPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
+    {
+    }
+
+    public List<UserFromTableModel> GetUsersFromTable()
+    {
+        IEnumerable<List<string>> tableData = GetTableRowsData();
+        List<UserFromTableModel> users = tableData.Select(row =>
+            new UserFromTableModel()
+            {
+                Email = row[0],
+                Figures = int.Parse(row[1]),
+                VerseChoreographies = int.Parse(row[2]),
+                FullChoreographies = int.Parse(row[3]),
+            })
+            .ToList();
+
+        return users;
+    }
+}
