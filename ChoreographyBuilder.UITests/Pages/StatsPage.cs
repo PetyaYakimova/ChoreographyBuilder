09a89bfff7ac3238c12245465aa726717f94da5c
Petya Yakimova <petya_yakimova@abv.bg>
2025-07-30 08:35:03 +0300

    Add stats page

---
+++ ChoreographyBuilder.UITests/Pages/StatsPage.cs
@@ -0,0 +1,15 @@
+﻿using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class StatsPage : BasePage
+{
+    public StatsPage(AppSettings settings, IWebDriver driver, WebDriverWait wait) : base(settings, driver, wait)
+    {
+    }
+
+    public string GetValueForLabel(string label)
+        => driver.FindElement(By.XPath($"//label[starts-with(text(), '{label}')]/../p")).Text;
+}
