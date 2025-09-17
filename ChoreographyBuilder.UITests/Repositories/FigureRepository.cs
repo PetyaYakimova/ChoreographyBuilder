07b5c4ffba6b6074b313fc84188d89b927ca1a67
Petya Yakimova <petya_yakimova@abv.bg>
2025-09-17 08:21:39 +0300

    Add figure repository

---
+++ ChoreographyBuilder.UITests/Repositories/FigureRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChoreographyBuilder.Infrastructure.Data.Models;
+using ChoreographyBuilder.UITests.Setup;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class FigureRepository : BaseRepository
+{
+    public FigureRepository(AppSettings settings) : base(settings)
+    {
+    }
+
+    public Figure? GetFigureByName(string name)
+    {
+        UpdateContext();
+        return context.Figures.FirstOrDefault(f => f.Name == name);
+    }
+}
