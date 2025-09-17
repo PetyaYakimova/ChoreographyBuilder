9ddd7448f57c3e14139ec472c20b383293f995fd
Petya Yakimova <petya_yakimova@abv.bg>
2025-09-17 08:21:23 +0300

    Add figure page

---
+++ ChoreographyBuilder.UITests/Pages/FigurePage.cs
@@ -0,0 +1,20 @@
+﻿using ChoreographyBuilder.Infrastructure.Data.Models;
+using ChoreographyBuilder.UITests.Repositories;
+using ChoreographyBuilder.UITests.Setup;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ChoreographyBuilder.UITests.Pages;
+
+public class FigurePage : BasePage
+{
+    private FigureRepository figureRepository;
+
+    public FigurePage(AppSettings settings, IWebDriver driver, WebDriverWait wait, FigureRepository figureRepository) : base(settings, driver, wait)
+    {
+        this.figureRepository = figureRepository;
+    }
+
+    public Figure? GetFigureFromDbByName(string name)
+        => figureRepository.GetFigureByName(name);
+}
