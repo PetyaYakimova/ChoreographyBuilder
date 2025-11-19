b17c53e3b4abd0853b4ce50a5ed5d6a58da3332f
Petya Yakimova <petya_yakimova@abv.bg>
2025-11-19 08:55:04 +0200

    Add step definitions for figure option exists

--- ChoreographyBuilder.UITests/StepDefinitions/FigureStepDefinitions.cs
+++ ChoreographyBuilder.UITests/StepDefinitions/FigureStepDefinitions.cs
@@ -31,4 +31,19 @@ public class FigureStepDefinitions : BaseStepDefinitions
         });
     }
 
+    [Then(@"I have asserted that a figure option for figure with name (.*), that has (.*) beats counts, that has start position (.*), that has end position (.*) and has dynamics type (.*) exists")]
+    public void AssertFigureOptionWithDataExists(string figureName, int beatsCount, string startPosition, string endPosition, string dynamicsType)
+    {
+        FigureOption? figure = figurePage.GetFigureFromDbByName(name);
+
+        Assert.NotNull(figure, $"Figure with name '{name}' does not exist in the database.");
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(figure.IsHighlight, Is.EqualTo(isHighlight));
+            Assert.That(figure.IsFavourite, Is.EqualTo(isFavourite));
+            Assert.That(figure.CanBeShared, Is.EqualTo(isShared));
+        });
+    }
+
 }
