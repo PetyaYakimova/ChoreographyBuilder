dca2e37d12e3e4f563f9b1156670e31e778c6169
Petya Yakimova <petya_yakimova@abv.bg>
2025-11-19 08:55:16 +0200

    Add figure options repository

---
+++ ChoreographyBuilder.UITests/Repositories/FigureOptionRepository.cs
@@ -0,0 +1,17 @@
+﻿using ChoreographyBuilder.Infrastructure.Data.Models;
+using ChoreographyBuilder.UITests.Setup;
+
+namespace ChoreographyBuilder.UITests.Repositories;
+
+public class FigureOptionRepository : BaseRepository
+{
+    public FigureOptionRepository(AppSettings settings) : base(settings)
+    {
+    }
+
+    public FigureOption? GetFigureOptionByFigureNameAndBeatsCount(string figureName, int beatsCount)
+    {
+        UpdateContext();
+        return context.FigureOptions.Where(fo => fo.Figure.Name == figureName).FirstOrDefault(fo => fo.BeatCounts == beatsCount);
+    }
+}
