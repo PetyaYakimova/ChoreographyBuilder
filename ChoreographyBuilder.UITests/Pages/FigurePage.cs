b510a60732cce486966754e6a2f9fad640016891
Petya Yakimova <petya_yakimova@abv.bg>
2025-11-20 08:38:18 +0200

    Update figure page

--- ChoreographyBuilder.UITests/Pages/FigurePage.cs
+++ ChoreographyBuilder.UITests/Pages/FigurePage.cs
@@ -9,12 +9,17 @@ namespace ChoreographyBuilder.UITests.Pages;
 public class FigurePage : BasePage
 {
     private FigureRepository figureRepository;
+    private FigureOptionRepository figureOptionRepository;
 
-    public FigurePage(AppSettings settings, IWebDriver driver, WebDriverWait wait, FigureRepository figureRepository) : base(settings, driver, wait)
+    public FigurePage(AppSettings settings, IWebDriver driver, WebDriverWait wait, FigureRepository figureRepository, FigureOptionRepository figureOptionRepository) : base(settings, driver, wait)
     {
         this.figureRepository = figureRepository;
+        this.figureOptionRepository = figureOptionRepository;
     }
 
     public Figure? GetFigureFromDbByName(string name)
         => figureRepository.GetFigureByName(name);
+
+    public FigureOption? GetFigureOptionFromDbByFigureNameAndBeatsCount(string figureName, int beatsCount)
+        => figureOptionRepository.GetFigureOptionByFigureNameAndBeatsCount(figureName, beatsCount);
 }
