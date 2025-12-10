d93f501ec910d9f3a54411cb78319d55fb37da9a
Petya Yakimova <petya_yakimova@abv.bg>
2025-12-10 08:51:41 +0200

    Update figure option repository

--- ChoreographyBuilder.UITests/Repositories/FigureOptionRepository.cs
+++ ChoreographyBuilder.UITests/Repositories/FigureOptionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ChoreographyBuilder.Infrastructure.Data.Models;
 using ChoreographyBuilder.UITests.Setup;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChoreographyBuilder.UITests.Repositories;
 
@@ -12,6 +13,11 @@ public class FigureOptionRepository : BaseRepository
     public FigureOption? GetFigureOptionByFigureNameAndBeatsCount(string figureName, int beatsCount)
     {
         UpdateContext();
-        return context.FigureOptions.Where(fo => fo.Figure.Name == figureName).FirstOrDefault(fo => fo.BeatCounts == beatsCount);
+        return context.FigureOptions
+            .Include(fo => fo.Figure)
+            .Include(fo => fo.StartPosition)
+            .Include(fo => fo.EndPosition)
+            .Where(fo => fo.Figure.Name == figureName)
+            .FirstOrDefault(fo => fo.BeatCounts == beatsCount);
     }
 }
